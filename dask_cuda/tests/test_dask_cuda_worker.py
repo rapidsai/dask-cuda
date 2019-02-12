82016a904b9977816c743b1a235a29e939a1717e
Adam Beberg <abeberg@nvidia.com>
2019-02-12 15:36:12 -0800

    fix tests

--- dask_cuda/tests/test_dask_cuda_worker.py
+++ dask_cuda/tests/test_dask_cuda_worker.py
@@ -15,35 +15,38 @@ from distributed.utils_test import popen, slow, terminate_process, wait_for_port
 from distributed.utils_test import loop  # noqa: F401
 
 
-def test_nanny_worker_ports(loop):
+def test_cuda_visible_devices(loop):
     os.environ["CUDA_VISIBLE_DEVICES"] = "2,3,7,8"
-    with popen(["dask-scheduler", "--port", "9359", "--no-bokeh"]) as sched:
-        with popen(
-            [
-                "dask-cuda-worker",
-                "127.0.0.1:9359",
-                "--host",
-                "127.0.0.1",
-                "--nprocs",
-                "4",
-                "--no-bokeh",
-            ]
-        ) as worker:
-            with Client("127.0.0.1:9359", loop=loop) as c:
-                start = time()
-                while True:
-                    if len(c.scheduler_info()["workers"]) == 4:
-                        break
-                    else:
-                        assert time() - start < 10
-                        sleep(0.1)
+    try:
+        with popen(["dask-scheduler", "--port", "9359", "--no-bokeh"]) as sched:
+            with popen(
+                [
+                    "dask-cuda-worker",
+                    "127.0.0.1:9359",
+                    "--host",
+                    "127.0.0.1",
+                    "--nprocs",
+                    "4",
+                    "--no-bokeh",
+                ]
+            ) as worker:
+                with Client("127.0.0.1:9359", loop=loop) as client:
+                    start = time()
+                    while True:
+                        if len(client.scheduler_info()["workers"]) == 4:
+                            break
+                        else:
+                            assert time() - start < 10
+                            sleep(0.1)
 
-                def get_visible_devices():
-                    return os.environ["CUDA_VISIBLE_DEVICES"]
+                    def get_visible_devices():
+                        return os.environ["CUDA_VISIBLE_DEVICES"]
 
-                # verify 4 workers with the 4 expected CUDA_VISIBLE_DEVICES
-                result = c.run(get_visible_devices)
-                expected = {"2,3,7,8": 1, "3,7,8,2": 1, "7,8,2,3": 1, "8,2,3,7": 1}
-                for v in result.values():
-                    del expected[v]
-                assert len(expected) == 0
+                    # verify 4 workers with the 4 expected CUDA_VISIBLE_DEVICES
+                    result = client.run(get_visible_devices)
+                    expected = {"2,3,7,8": 1, "3,7,8,2": 1, "7,8,2,3": 1, "8,2,3,7": 1}
+                    for v in result.values():
+                        del expected[v]
+                    assert len(expected) == 0
+    finally:
+        del os.environ["CUDA_VISIBLE_DEVICES"]
