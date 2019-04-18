1aa68a8af643284d0c7650035acd02e109b57c5e
Peter Andreas Entschev <peter@entschev.com>
2019-04-18 14:53:09 +0200

    Add support for yaml configurations

--- dask_cuda/__init__.py
+++ dask_cuda/__init__.py
@@ -1,1 +1,2 @@
 from .local_cuda_cluster import LocalCUDACluster
+from . import config
---
+++ dask_cuda/config.py
@@ -0,0 +1,13 @@
+import yaml
+import os
+
+import dask
+
+config = dask.config.config
+
+
+fn = os.path.join(os.path.dirname(__file__), 'dask_cuda.yaml')
+with open(fn) as f:
+    dask_cuda_defaults = yaml.load(f)
+
+dask.config.update_defaults(dask_cuda_defaults)
