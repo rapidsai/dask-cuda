b3cbf2ce74d53bfd749b5033edc69d909cc69f16
Peter Andreas Entschev <peter@entschev.com>
2019-04-23 18:12:07 +0200

    Add assert_device_host_file_size in utils_test module

---
+++ dask_cuda/utils_test.py
@@ -0,0 +1,14 @@
+import os
+from zict.file import _safe_key as safe_key
+
+def assert_device_host_file_size(dhf, total_bytes, chunk_overhead=1024):
+    byte_sum = dhf.device.fast.total_weight + dhf.host.fast.total_weight
+    file_path = [os.path.join(dhf.host.slow.d.directory, safe_key(k)) for k in
+                 dhf.host.slow.keys()]
+    file_size = [os.path.getsize(f) for f in file_path]
+    byte_sum += sum(file_size)
+    print(file_size)
+
+    # Allow up to chunk_overhead bytes overhead per chunk on disk
+    assert (byte_sum >= total_bytes
+            and byte_sum <= total_bytes + len(dhf.host.slow) * chunk_overhead)
