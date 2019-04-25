1dd2c16a3be37da7f994e92638e91d40ee8e2498
Peter Andreas Entschev <peter@entschev.com>
2019-04-25 22:23:34 +0200

    Add DeviceHostFile test

---
+++ dask_cuda/tests/test_device_host_file.py
@@ -0,0 +1,105 @@
+import numpy as np
+import cupy
+from dask_cuda.device_host_file import DeviceHostFile
+from random import randint
+
+import pytest
+from dask_cuda.utils_test import gen_random_key
+from cupy.testing import assert_array_equal
+
+
+@pytest.mark.parametrize("num_host_arrays", [1, 10, 100])
+@pytest.mark.parametrize("num_device_arrays", [1, 10, 100])
+@pytest.mark.parametrize("array_size_range", [(1, 1000), (100, 100), (1000, 1000)])
+def test_device_host_file_short(num_device_arrays, num_host_arrays, array_size_range):
+    import tempfile
+    tmpdir = tempfile.TemporaryDirectory()
+    dhf = DeviceHostFile(device_memory_limit=1024 * 16,
+                         memory_limit=1024 * 16,
+                         local_dir=tmpdir.name)
+
+    host = [(gen_random_key(64), np.random.random(randint(*array_size_range)))
+            for i in range(num_host_arrays)]
+    device = [(gen_random_key(64), cupy.random.random(randint(*array_size_range)))
+              for i in range(num_device_arrays)]
+
+    import random
+    full = host + device
+    random.shuffle(full)
+
+    for i in full:
+        dhf[i[0]] = i[1]
+
+    random.shuffle(full)
+
+    for i in full:
+        assert_array_equal(i[1], dhf[i[0]])
+        del dhf[i[0]]
+
+    assert set(dhf.device.fast.keys()) == set()
+    assert set(dhf.host.fast.keys()) == set()
+    assert set(dhf.host.slow.keys()) == set()
+
+
+def test_device_host_file_step_by_step():
+    import tempfile
+    tmpdir = tempfile.TemporaryDirectory()
+    dhf = DeviceHostFile(device_memory_limit=1024 * 16, memory_limit=1024 * 16,
+                         local_dir=tmpdir.name)
+
+    a = np.random.random(1000)
+    b = cupy.random.random(1000)
+
+    dhf["a1"] = a
+
+    assert set(dhf.device.fast.keys()) == set()
+    assert set(dhf.host.fast.keys()) == set(["a1"])
+    assert set(dhf.host.slow.keys()) == set()
+
+    dhf["b1"] = b
+
+    assert set(dhf.device.fast.keys()) == set(["b1"])
+    assert set(dhf.host.fast.keys()) == set(["a1"])
+    assert set(dhf.host.slow.keys()) == set()
+
+    dhf["b2"] = b
+    assert set(dhf.device.fast.keys()) == set(["b1", "b2"])
+    assert set(dhf.host.fast.keys()) == set(["a1"])
+    assert set(dhf.host.slow.keys()) == set()
+
+    dhf["b3"] = b
+    assert set(dhf.device.fast.keys()) == set(["b2", "b3"])
+    assert set(dhf.host.fast.keys()) == set(["a1", "b1"])
+    assert set(dhf.host.slow.keys()) == set()
+
+    dhf["a2"] = a
+    assert set(dhf.device.fast.keys()) == set(["b2", "b3"])
+    assert set(dhf.host.fast.keys()) == set(["a2", "b1"])
+    assert set(dhf.host.slow.keys()) == set(["a1"])
+
+    dhf["b4"] = b
+    assert set(dhf.device.fast.keys()) == set(["b3", "b4"])
+    assert set(dhf.host.fast.keys()) == set(["a2", "b2"])
+    assert set(dhf.host.slow.keys()) == set(["a1", "b1"])
+
+    dhf["b4"] = b
+    assert set(dhf.device.fast.keys()) == set(["b3", "b4"])
+    assert set(dhf.host.fast.keys()) == set(["a2", "b2"])
+    assert set(dhf.host.slow.keys()) == set(["a1", "b1"])
+
+    assert_array_equal(dhf["a1"], a)
+    del dhf.device["a1"]
+    assert_array_equal(dhf["a2"], a)
+    del dhf.device["a2"]
+    assert_array_equal(dhf["b1"], b)
+    del dhf.device["b1"]
+    assert_array_equal(dhf["b2"], b)
+    del dhf.device["b2"]
+    assert_array_equal(dhf["b3"], b)
+    del dhf.device["b3"]
+    assert_array_equal(dhf["b4"], b)
+    del dhf.device["b4"]
+
+    assert set(dhf.device.fast.keys()) == set()
+    assert set(dhf.host.fast.keys()) == set()
+    assert set(dhf.host.slow.keys()) == set()
--- dask_cuda/utils_test.py
+++ dask_cuda/utils_test.py
@@ -1,5 +1,8 @@
 import os
 from zict.file import _safe_key as safe_key
+import string
+from random import choice
+
 
 def assert_device_host_file_size(dhf, total_bytes, chunk_overhead=1024):
     byte_sum = dhf.device.fast.total_weight + dhf.host.fast.total_weight
@@ -12,3 +15,8 @@ def assert_device_host_file_size(dhf, total_bytes, chunk_overhead=1024):
     # Allow up to chunk_overhead bytes overhead per chunk on disk
     assert (byte_sum >= total_bytes
             and byte_sum <= total_bytes + len(dhf.host.slow) * chunk_overhead)
+
+
+def gen_random_key(key_length):
+    chars = string.ascii_letters + string.digits + '%_-'
+    return ''.join(choice(chars) for _ in range(key_length))
