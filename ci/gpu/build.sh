8659fb6a01bd532663d3a1cffb67ffb8254b013f
Peter Andreas Entschev <peter@entschev.com>
2019-04-29 14:46:18 +0200

    Fixes for GPU CI
    * Install upstream packages after conda environment activation
    * Install NumPy/Numba packages with conda recipes

--- ci/gpu/build.sh
+++ ci/gpu/build.sh
@@ -24,21 +24,6 @@ export GIT_DESCRIBE_NUMBER=`git rev-list ${GIT_DESCRIBE_TAG}..HEAD --count`
 export NUMPY_EXPERIMENTAL_ARRAY_FUNCTION=1
 
 ################################################################################
-# SETUP - Install additional packages
-################################################################################
-
-# Use dask master until there's a release with
-# https://github.com/dask/dask/pull/4715
-pip install --upgrade git+https://github.com/dask/dask
-
-# Use dask-distributed master until there's a release with
-# https://github.com/dask/distributed/pull/2625
-pip install --upgrade git+https://github.com/dask/distributed
-
-# Install CuPy for tests
-pip install cupy-cuda100==6.0.0rc1
-
-################################################################################
 # SETUP - Check environment
 ################################################################################
 
@@ -56,6 +41,21 @@ conda list
 
 # FIX Added to deal with Anancoda SSL verification issues during conda builds
 conda config --set ssl_verify False
+
+################################################################################
+# SETUP - Install additional packages
+################################################################################
+
+# Use dask master until there's a release with
+# https://github.com/dask/dask/pull/4715
+pip install --upgrade git+https://github.com/dask/dask
+
+# Use dask-distributed master until there's a release with
+# https://github.com/dask/distributed/pull/2625
+pip install --upgrade git+https://github.com/dask/distributed
+
+# Install CuPy for tests
+pip install cupy-cuda100==6.0.0rc1
 
 ################################################################################
 # TEST - Run tests
