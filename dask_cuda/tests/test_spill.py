2c24f033683d7847904bb465458fb376f56bd24f
Peter Andreas Entschev <peter@entschev.com>
2019-04-29 21:44:10 +0200

    Fix test_spill, extend it to multiple parameters

--- dask_cuda/tests/test_spill.py
+++ dask_cuda/tests/test_spill.py
@@ -3,47 +3,60 @@ from distributed import wait
 from dask_cuda.cuda_worker import CUDAWorker
 from dask_cuda.utils_test import assert_device_host_file_size as assert_dhf_size
 import dask.array as da
+import pytest
 
 
-@gen_cluster(
-    client=True,
-    ncores=[("127.0.0.1", 1)],
-    Worker=CUDAWorker,
-    worker_kwargs={"device_memory_limit": 2e9, "memory_limit": 1e9},
-    config={
-        "cuda.worker.device-memory.target": 0.2,
-        "cuda.worker.device-memory.spill": 0.2
-    },
-)
-def test_device_spill(client, scheduler, worker):
-    import cupy
-    cupy.cuda.set_allocator(None)
-    cupy.cuda.set_pinned_memory_allocator(None)
+@pytest.mark.parametrize("params", [
+    {"device_memory_limit" : 2e9,
+     "memory_limit": 4e9,
+     "host_target": 0.6,
+     "host_spill": 0.7,
+     "spills_to_disk": False},
+    {"device_memory_limit" : 2e9,
+     "memory_limit": 1e9,
+     "host_target": 0.3,
+     "host_spill": 0.3,
+     "spills_to_disk": True}
+])
+@pytest.mark.parametrize("device_limit", [0.3, 0.5])
+def test_device_spill(params, device_limit):
+    @gen_cluster(
+        client=True,
+        ncores=[("127.0.0.1", 1)],
+        Worker=CUDAWorker,
+        worker_kwargs={"device_memory_limit": params["device_memory_limit"],
+                       "memory_limit": params["memory_limit"]},
+        config={
+            "distributed.worker.memory.target": params["host_target"],
+            "distributed.worker.memory.spill": params["host_spill"],
+            "cuda.worker.device-memory.target": device_limit,
+            "cuda.worker.device-memory.spill": device_limit
+        },
+    )
+    def test_device_spill(client, scheduler, worker):
+        import cupy
+        cupy.cuda.set_allocator(None)
+        cupy.cuda.set_pinned_memory_allocator(None)
 
-    rs = da.random.RandomState(RandomState=cupy.random.RandomState)
-    x = rs.random(int(200e6 * 0.7), chunks=5e6)
-    yield wait(x)
+        rs = da.random.RandomState(RandomState=cupy.random.RandomState)
+        x = rs.random(int(200e6 * 0.7), chunks=10e6)
+        yield wait(x)
 
-    xx = x.persist()
-    yield wait(xx)
+        xx = x.persist()
+        yield wait(xx)
 
-    # Allow up to 1024 bytes overhead per chunk on disk
-    assert_dhf_size(worker.data, x.nbytes, 1024)
+        # Allow up to 1024 bytes overhead per chunk serialized
+        assert_dhf_size(worker.data, x.nbytes, 1024)
 
-    # At this point, some data must be on the host, but not necessarily
-    # on device or disk
-    assert worker.data.host.fast.total_weight > 0
+        y = client.compute(x.sum())
+        res = yield y
 
-    y = client.compute(x.sum())
-    res = yield y
+        assert (abs(res / x.size) - 0.5) < 1e-3
 
-    assert (abs(res / x.size) - 0.5) < 1e-3
+        assert_dhf_size(worker.data, x.nbytes, 1024)
+        if params["spills_to_disk"]:
+            assert len(worker.data.host.slow) > 0
+        else:
+            assert len(worker.data.host.slow) == 0
 
-    assert_dhf_size(worker.data, x.nbytes, 1024)
-
-    # Here no data should be on the device (may contain a small
-    # 'finalize' task only, < 64 bytes), both host and disk should
-    # contain some data
-    assert worker.data.device.fast.total_weight < 64
-    assert worker.data.host.fast.total_weight > 0
-    assert len(worker.data.host.slow) > 0
+    test_device_spill()
