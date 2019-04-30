ce5c650b21cb255043125fdca152297f74354b86
Peter Andreas Entschev <peter@entschev.com>
2019-04-30 08:58:07 -0700

    Add single-GPU test for visible devices
    Multi-GPU test will run if there are multiple GPUs

--- dask_cuda/tests/test_dask_cuda_worker.py
+++ dask_cuda/tests/test_dask_cuda_worker.py
@@ -14,9 +14,12 @@ from distributed.utils import sync, tmpfile
 from distributed.utils_test import popen, slow, terminate_process, wait_for_port
 from distributed.utils_test import loop  # noqa: F401
 
+from dask_cuda.utils import get_n_gpus
+from dask_cuda.local_cuda_cluster import cuda_visible_devices
 
-def test_cuda_visible_devices(loop):
-    os.environ["CUDA_VISIBLE_DEVICES"] = "2,3,6,7"
+
+def test_cuda_visible_devices_single(loop):
+    os.environ["CUDA_VISIBLE_DEVICES"] = "0"
     try:
         with popen(["dask-scheduler", "--port", "9359", "--no-bokeh"]) as sched:
             with popen(
@@ -31,7 +34,7 @@ def test_cuda_visible_devices(loop):
                 with Client("127.0.0.1:9359", loop=loop) as client:
                     start = time()
                     while True:
-                        if len(client.scheduler_info()["workers"]) == 4:
+                        if len(client.scheduler_info()["workers"]) == 1:
                             break
                         else:
                             assert time() - start < 10
@@ -42,7 +45,47 @@ def test_cuda_visible_devices(loop):
 
                     # verify 4 workers with the 4 expected CUDA_VISIBLE_DEVICES
                     result = client.run(get_visible_devices)
-                    expected = {"2,3,6,7": 1, "3,6,7,2": 1, "6,7,2,3": 1, "7,2,3,6": 1}
+                    expected = {"0": 1}
+                    for v in result.values():
+                        del expected[v]
+                    assert len(expected) == 0
+    finally:
+        del os.environ["CUDA_VISIBLE_DEVICES"]
+
+
+def test_cuda_visible_devices(loop):
+    n_gpus = get_n_gpus()
+    if n_gpus < 2:
+        pytest.skip("More than 1 GPU required for test")
+    os.environ["CUDA_VISIBLE_DEVICES"] = cuda_visible_devices(0)
+    try:
+        with popen(["dask-scheduler", "--port", "9359", "--no-bokeh"]) as sched:
+            with popen(
+                [
+                    "dask-cuda-worker",
+                    "127.0.0.1:9359",
+                    "--host",
+                    "127.0.0.1",
+                    "--no-bokeh",
+                ]
+            ) as worker:
+                with Client("127.0.0.1:9359", loop=loop) as client:
+                    start = time()
+                    while True:
+                        if len(client.scheduler_info()["workers"]) == n_gpus:
+                            break
+                        else:
+                            assert time() - start < 10
+                            sleep(0.1)
+
+                    def get_visible_devices():
+                        return os.environ["CUDA_VISIBLE_DEVICES"]
+
+                    # verify n_gpus workers with CUDA_VISIBLE_DEVICES
+                    # in proper order
+                    result = client.run(get_visible_devices)
+                    expected = {cuda_visible_devices(i, [j for j in
+                        range(n_gpus)]): 1 for i in range(n_gpus)}
                     for v in result.values():
                         del expected[v]
                     assert len(expected) == 0
