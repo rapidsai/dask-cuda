388c6773d090c000f5491b740e2092b39652bee5
Matthew Rocklin <mrocklin@gmail.com>
2019-05-02 11:39:12 -0700

    Use yaml.safe_load

--- dask_cuda/config.py
+++ dask_cuda/config.py
@@ -6,8 +6,9 @@ import dask
 config = dask.config.config
 
 
-fn = os.path.join(os.path.dirname(__file__), 'cuda.yaml')
+fn = os.path.join(os.path.dirname(__file__), "cuda.yaml")
+dask.config.ensure_file(source=fn)
 with open(fn) as f:
-    dask_cuda_defaults = yaml.load(f)
+    dask_cuda_defaults = yaml.safe_load(f)
 
 dask.config.update_defaults(dask_cuda_defaults)
