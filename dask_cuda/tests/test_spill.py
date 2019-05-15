6428b5478820f1e95097ca3a7e18ca6cf1fe0684
Peter Andreas Entschev <peter@entschev.com>
2019-05-15 21:37:48 +0200

    Add LocalCUDACluster device spilling test

--- dask_cuda/tests/test_spill.py
+++ dask_cuda/tests/test_spill.py
@@ -1,8 +1,10 @@
-from distributed.utils_test import gen_cluster
+from distributed.utils_test import gen_cluster, loop, gen_test
 from distributed.worker import Worker
-from distributed import wait
+from distributed import Client, get_worker, wait
+from dask_cuda import LocalCUDACluster
 from dask_cuda.device_host_file import DeviceHostFile
 import dask.array as da
+import cupy
 import pytest
 import os
 from zict.file import _safe_key as safe_key
@@ -60,11 +62,6 @@ def test_device_spill(params):
         },
     )
     def test_device_spill(client, scheduler, worker):
-        import cupy
-
-        cupy.cuda.set_allocator(None)
-        cupy.cuda.set_pinned_memory_allocator(None)
-
         rs = da.random.RandomState(RandomState=cupy.random.RandomState)
         x = rs.random(int(250e6), chunks=10e6)
         yield wait(x)
@@ -72,14 +69,11 @@ def test_device_spill(params):
         xx = x.persist()
         yield wait(xx)
 
-        print(worker.data.device_buffer)
-
         # Allow up to 1024 bytes overhead per chunk serialized
         assert_device_host_file_size(worker.data, x.nbytes, 1024)
 
         y = client.compute(x.sum())
         res = yield y
-        print(worker.data.device_buffer)
 
         assert (abs(res / x.size) - 0.5) < 1e-3
 
@@ -90,3 +84,66 @@ def test_device_spill(params):
             assert len(worker.data.disk) == 0
 
     test_device_spill()
+
+
+@pytest.mark.parametrize(
+    "params",
+    [
+        {
+            "device_memory_limit": 2e9,
+            "memory_limit": 4e9,
+            "host_target": 0.6,
+            "host_spill": 0.7,
+            "spills_to_disk": False,
+        },
+        {
+            "device_memory_limit": 1e9,
+            "memory_limit": 1e9,
+            "host_target": 0.3,
+            "host_spill": 0.3,
+            "spills_to_disk": True,
+        },
+    ],
+)
+def test_cluster_device_spill(loop, params):
+    @gen_test()
+    def test_cluster_device_spill():
+        cluster = yield LocalCUDACluster(
+            1,
+            scheduler_port=0,
+            processes=True,
+            silence_logs=False,
+            dashboard_address=None,
+            asynchronous=True,
+            device_memory_limit=params["device_memory_limit"],
+            memory_limit=params["memory_limit"],
+            memory_target_fraction=params["host_target"],
+            memory_spill_fraction=params["host_spill"],
+        )
+        client = yield Client(cluster, asynchronous=True)
+
+        rs = da.random.RandomState(RandomState=cupy.random.RandomState)
+        x = rs.random(int(250e6), chunks=10e6)
+        yield wait(x)
+
+        xx = x.persist()
+        yield wait(xx)
+
+        def get_data(worker, total_size):
+            assert_device_host_file_size(get_worker().data, total_size)
+
+        # Allow up to 1024 bytes overhead per chunk serialized
+        yield client.run(get_data, cluster.workers[0].Worker, x.nbytes)
+
+        y = client.compute(x.sum())
+        res = yield y
+
+        assert (abs(res / x.size) - 0.5) < 1e-3
+
+        yield client.run(get_data, cluster.workers[0].Worker, x.nbytes)
+        disk_chunks = yield client.run(lambda: len(get_worker().data.disk))
+        for dc in disk_chunks.values():
+            if params["spills_to_disk"]:
+                assert dc > 0
+            else:
+                assert dc == 0
