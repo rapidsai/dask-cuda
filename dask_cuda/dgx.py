a149a500a2c6a8cdc838f5ad6ba54f2e87bba286
Matthew Rocklin <mrocklin@gmail.com>
2019-05-24 07:41:02 -0700

    Add docstring for DGX

--- dask_cuda/dgx.py
+++ dask_cuda/dgx.py
@@ -25,17 +25,61 @@ affinity = {  # nvidia-smi topo -m
 }
 
 
-def DGX(interface="ib", dashboard_address=":8787", threads_per_worker=1, **kwargs):
-    gpus = list(
-        map(int, os.environ.get("CUDA_VISIBLE_DEVICES", "0,1,2,3,4,5,6,7").split(","))
-    )
+def DGX(
+    interface="ib",
+    dashboard_address=":8787",
+    threads_per_worker=1,
+    silence_logs=True,
+    CUDA_VISIBLE_DEVICES=None,
+    **kwargs
+):
+    """ A Local Cluster for a DGX 1 machine
+
+    NVIDIA's DGX-1 machine has a complex architecture mapping CPUs, GPUs, and
+    network hardware.  This function creates a local cluster that tries to
+    respect this hardware as much as possible.
+
+    It creates one Dask worker process per GPU, and assigns each worker process
+    the correct CPU cores and Network interface cards to maximize performance.
+
+    That being said, things aren't perfect.  Today a DGX has very high
+    performance between certain sets of GPUs and not others.  A Dask DGX
+    cluster that uses only certain tightly coupled parts of the computer will
+    have significantly higher bandwidth than a deployment on the entire thing.
+
+    Parameters
+    ----------
+    interface: str
+        The interface prefix for the infiniband networking cards.  This is
+        often "ib"` or "bond".  We will add the numeric suffix 0,1,2,3 as
+        appropriate.  Defaults to "ib".
+    dashboard_address: str
+        The address for the scheduler dashboard.  Defaults to ":8787".
+    CUDA_VISIBLE_DEVICES: str
+        String like ``"0,1,2,3"`` or ``[0, 1, 2, 3]`` to restrict activity to
+        different GPUs
+
+    Examples
+    --------
+    >>> from dask_cuda import DGX
+    >>> from dask.distributed import Client
+    >>> cluster = DGX(interface='ib')
+    >>> client = Client(cluster)
+    """
+    if CUDA_VISIBLE_DEVICES is None:
+        CUDA_VISIBLE_DEVICES = os.environ.get("CUDA_VISIBLE_DEVICES", "0,1,2,3,4,5,6,7")
+    if isinstance(CUDA_VISIBLE_DEVICES, str):
+        CUDA_VISIBLE_DEVICES = CUDA_VISIBLE_DEVICES.split(",")
+    CUDA_VISIBLE_DEVICES = list(map(int, CUDA_VISIBLE_DEVICES))
 
     spec = {
         i: {
             "cls": Nanny,
             "options": {
                 "env": {
-                    "CUDA_VISIBLE_DEVICES": cuda_visible_devices(ii, gpus),
+                    "CUDA_VISIBLE_DEVICES": cuda_visible_devices(
+                        ii, CUDA_VISIBLE_DEVICES
+                    ),
                     # 'UCX_NET_DEVICES': 'mlx5_%d:1' % (i // 2)
                     "UCX_TLS": "rc,cuda_copy,cuda_ipc",
                 },
@@ -46,18 +90,21 @@ def DGX(interface="ib", dashboard_address=":8787", threads_per_worker=1, **kwarg
                 "preload": ["dask_cuda.initialize_context"],
                 "dashboard_address": ":0",
                 "plugins": [CPUAffinity(affinity[i])],
+                "silence_logs": silence_logs,
             },
         }
-        for ii, i in enumerate(gpus)
+        for ii, i in enumerate(CUDA_VISIBLE_DEVICES)
     }
 
     scheduler = {
         "cls": Scheduler,
         "options": {
-            "interface": interface + str(gpus[0] // 2),
+            "interface": interface + str(CUDA_VISIBLE_DEVICES[0] // 2),
             "protocol": "ucx",
             "dashboard_address": dashboard_address,
         },
     }
 
-    return SpecCluster(workers=spec, scheduler=scheduler, **kwargs)
+    return SpecCluster(
+        workers=spec, scheduler=scheduler, silence_logs=silence_logs, **kwargs
+    )
