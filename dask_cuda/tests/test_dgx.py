e171d17128c90d99e6e9e3b7241cc665cf4b9f96
Matthew Rocklin <mrocklin@gmail.com>
2019-05-28 16:08:07 -0500

    Skip dgx test if no interface ib0

--- dask_cuda/tests/test_dgx.py
+++ dask_cuda/tests/test_dgx.py
@@ -1,9 +1,15 @@
+import psutil
+import pytest
 from distributed.utils_test import gen_test
 
 from dask.distributed import Client
 from dask_cuda import DGX
 
 
+@pytest.mark.skipif(
+    'ib0' not in psutil.net_if_addrs(),
+    reason='Infiniband interface ib0 not found'
+)
 @gen_test(timeout=20)
 async def test_dgx():
     async with DGX(asynchronous=True) as cluster:
