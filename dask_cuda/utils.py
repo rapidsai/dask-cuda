f6ab9828b44a1a5752b63d187470b27c14a9f7a6
Peter Andreas Entschev <peter@entschev.com>
2019-07-25 07:39:16 -0700

    Add utility functions to move serialized frames to/from device

--- dask_cuda/utils.py
+++ dask_cuda/utils.py
@@ -1,5 +1,6 @@
 import toolz
 import os
+import numpy as np
 from numba import cuda
 
 
@@ -21,3 +22,25 @@ def get_device_total_memory(index=0):
     """
     with cuda.gpus[index]:
         return cuda.current_context().get_memory_info()[1]
+
+
+def convert_frames_to_numba(frames):
+    try:
+        import cupy
+    except ImportError:
+        return frames
+
+    return [cuda.as_cuda_array(f) if isinstance(f, cupy.ndarray) else f for f in frames]
+
+
+def move_frames_to_host(frames):
+    # Conversion of frames to Numba can/should be eventually done during the
+    # serialization process in dask/distributed
+    frames = convert_frames_to_numba(frames)
+
+    return [f.copy_to_host() if isinstance(f, cuda.cudadrv.devicearray.DeviceNDArray)
+            else f for f in frames]
+
+
+def move_frames_to_device(frames):
+    return [cuda.to_device(f) if isinstance(f, np.ndarray) else f for f in frames]
