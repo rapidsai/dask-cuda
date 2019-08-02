95fa94c1047590c5ba186127f737745d31a54ee8
Peter Andreas Entschev <peter@entschev.com>
2019-08-02 23:02:36 +0200

    Improve device memory spilling performance (#98)
    The performance improvement is achieved by avoiding the pickling
    of objects between host and device, simply moving the data without
    that overhead.

--- dask_cuda/device_host_file.py
+++ dask_cuda/device_host_file.py
@@ -1,28 +1,94 @@
 from zict import Buffer, File, Func
 from zict.common import ZictBase
-from distributed.protocol import deserialize_bytes, serialize_bytes
+from distributed.protocol import (
+    deserialize,
+    deserialize_bytes,
+    serialize,
+    serialize_bytelist,
+    dask_serialize,
+    dask_deserialize,
+)
+from dask.sizeof import sizeof
+from distributed.utils import nbytes
 from distributed.worker import weight
 
-from functools import partial
+from numba import cuda
 import os
 
 from .is_device_object import is_device_object
 
 
-def _serialize_if_device(obj):
-    """ Serialize an object if it's a device object """
-    if is_device_object(obj):
-        return serialize_bytes(obj, on_error="raise")
-    else:
-        return obj
+# Register sizeof for Numba DeviceNDArray while Dask doesn't add it
+if not hasattr(sizeof, "register_numba"):
+
+    @sizeof.register_lazy("numba")
+    def register_numba():
+        import numba
+
+        @sizeof.register(numba.cuda.cudadrv.devicearray.DeviceNDArray)
+        def sizeof_numba_devicearray(x):
+            return int(x.nbytes)
 
 
-def _deserialize_if_device(obj):
-    """ Deserialize an object if it's an instance of bytes """
-    if isinstance(obj, bytes):
-        return deserialize_bytes(obj)
-    else:
-        return obj
+class DeviceSerialized:
+    """ Store device object on the host
+
+    This stores a device-side object as
+
+    1.  A msgpack encodable header
+    2.  A list of objects that are returned by calling
+        `numba.cuda.as_cuda_array(f).copy_to_host()`
+        which are typically NumPy arrays
+    """
+
+    def __init__(self, header, parts, is_cuda):
+        self.header = header
+        self.parts = parts
+        self.is_cuda = is_cuda
+
+    def __sizeof__(self):
+        return sum(map(nbytes, self.parts))
+
+
+@dask_serialize.register(DeviceSerialized)
+def _(obj):
+    headers = []
+    all_frames = []
+    for part in obj.parts:
+        header, frames = serialize(part)
+        header["frame-start-stop"] = [len(all_frames), len(all_frames) + len(frames)]
+        headers.append(header)
+        all_frames.extend(frames)
+
+    header = {"sub-headers": headers, "is-cuda": obj.is_cuda, "main-header": obj.header}
+
+    return header, all_frames
+
+
+@dask_deserialize.register(DeviceSerialized)
+def _(header, frames):
+    parts = []
+    for sub_header in header["sub-headers"]:
+        start, stop = sub_header.pop("frame-start-stop")
+        part = deserialize(sub_header, frames[start:stop])
+        parts.append(part)
+
+    return DeviceSerialized(header["main-header"], parts, header["is-cuda"])
+
+
+def device_to_host(obj: object) -> DeviceSerialized:
+    header, frames = serialize(obj, serializers=["cuda"])
+    is_cuda = [hasattr(f, "__cuda_array_interface__") for f in frames]
+    frames = [
+        cuda.as_cuda_array(f).copy_to_host() if ic else f
+        for ic, f in zip(is_cuda, frames)
+    ]
+    return DeviceSerialized(header, frames, is_cuda)
+
+
+def host_to_device(s: DeviceSerialized) -> object:
+    frames = [cuda.to_device(f) if ic else f for ic, f in zip(s.is_cuda, s.parts)]
+    return deserialize(s.header, frames)
 
 
 class DeviceHostFile(ZictBase):
@@ -55,17 +121,14 @@ class DeviceHostFile(ZictBase):
         path = os.path.join(local_directory, "storage")
 
         self.host_func = dict()
-        self.disk_func = Func(
-            partial(serialize_bytes, on_error="raise"), deserialize_bytes, File(path)
-        )
+        self.disk_func = Func(serialize_bytelist, deserialize_bytes, File(path))
         self.host_buffer = Buffer(
             self.host_func, self.disk_func, memory_limit, weight=weight
         )
 
+        self.device_keys = set()
         self.device_func = dict()
-        self.device_host_func = Func(
-            _serialize_if_device, _deserialize_if_device, self.host_buffer
-        )
+        self.device_host_func = Func(device_to_host, host_to_device, self.host_buffer)
         self.device_buffer = Buffer(
             self.device_func, self.device_host_func, device_memory_limit, weight=weight
         )
@@ -79,20 +142,18 @@ class DeviceHostFile(ZictBase):
 
     def __setitem__(self, key, value):
         if is_device_object(value):
+            self.device_keys.add(key)
             self.device_buffer[key] = value
         else:
             self.host_buffer[key] = value
 
     def __getitem__(self, key):
-        if key in self.host_buffer:
-            obj = self.host_buffer[key]
-            del self.host_buffer[key]
-            self.device_buffer[key] = _deserialize_if_device(obj)
-
-        if key in self.device_buffer:
+        if key in self.device_keys:
             return self.device_buffer[key]
+        elif key in self.host_buffer:
+            return self.host_buffer[key]
         else:
-            raise KeyError
+            raise KeyError(key)
 
     def __len__(self):
         return len(self.device_buffer)
@@ -100,5 +161,6 @@ class DeviceHostFile(ZictBase):
     def __iter__(self):
         return iter(self.device_buffer)
 
-    def __delitem__(self, i):
-        del self.device_buffer[i]
+    def __delitem__(self, key):
+        self.device_keys.discard(key)
+        del self.device_buffer[key]
--- dask_cuda/tests/test_device_host_file.py
+++ dask_cuda/tests/test_device_host_file.py
@@ -1,10 +1,11 @@
 import numpy as np
 import cupy
-from dask_cuda.device_host_file import DeviceHostFile
+from dask_cuda.device_host_file import DeviceHostFile, host_to_device, device_to_host
+from distributed.protocol import deserialize_bytes, serialize_bytelist
 from random import randint
+import dask.array as da
 
 import pytest
-from cupy.testing import assert_array_equal
 
 
 @pytest.mark.parametrize("num_host_arrays", [1, 10, 100])
@@ -33,14 +34,15 @@ def test_device_host_file_short(
     full = host + device
     random.shuffle(full)
 
-    for i in full:
-        dhf[i[0]] = i[1]
+    for k, v in full:
+        dhf[k] = v
 
     random.shuffle(full)
 
-    for i in full:
-        assert_array_equal(i[1], dhf[i[0]])
-        del dhf[i[0]]
+    for k, original in full:
+        acquired = dhf[k]
+        da.assert_eq(original, acquired)
+        del dhf[k]
 
     assert set(dhf.device.keys()) == set()
     assert set(dhf.host.keys()) == set()
@@ -94,19 +96,48 @@ def test_device_host_file_step_by_step(tmp_path):
     assert set(dhf.host.keys()) == set(["a2", "b2"])
     assert set(dhf.disk.keys()) == set(["a1", "b1"])
 
-    assert_array_equal(dhf["a1"], a)
+    da.assert_eq(dhf["a1"], a)
     del dhf["a1"]
-    assert_array_equal(dhf["a2"], a)
+    da.assert_eq(dhf["a2"], a)
     del dhf["a2"]
-    assert_array_equal(dhf["b1"], b)
+    da.assert_eq(dhf["b1"], b)
     del dhf["b1"]
-    assert_array_equal(dhf["b2"], b)
+    da.assert_eq(dhf["b2"], b)
     del dhf["b2"]
-    assert_array_equal(dhf["b3"], b)
+    da.assert_eq(dhf["b3"], b)
     del dhf["b3"]
-    assert_array_equal(dhf["b4"], b)
+    da.assert_eq(dhf["b4"], b)
     del dhf["b4"]
 
     assert set(dhf.device.keys()) == set()
     assert set(dhf.host.keys()) == set()
     assert set(dhf.disk.keys()) == set()
+
+
+def test_serialize_cupy():
+    x = cupy.arange(10)
+
+    obj = device_to_host(x)
+    assert all(isinstance(part, (bytes, memoryview, np.ndarray)) for part in obj.parts)
+    btslst = serialize_bytelist(obj)
+
+    bts = deserialize_bytes(b"".join(btslst))
+    y = host_to_device(bts)
+
+    da.assert_eq(x, y)
+
+
+def test_serialize_cudf():
+    cudf = pytest.importorskip("cudf")
+    dd = pytest.importorskip("dask.dataframe")
+
+    df = cudf.DataFrame({"x": [1, 2, 3], "y": [4.0, 5.0, 6.0]})
+
+    obj = device_to_host(df)
+    assert all(isinstance(part, (bytes, memoryview, np.ndarray)) for part in obj.parts)
+    btslst = serialize_bytelist(obj)
+
+    bts = deserialize_bytes(b"".join(btslst))
+    df2 = host_to_device(bts)
+
+    dd.assert_eq(df, df2)
