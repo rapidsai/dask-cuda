19778c4e2754e2d6f0e4e83d1d85f7d73c1a5923
Peter Andreas Entschev <peter@entschev.com>
2019-09-24 05:03:44 -0700

    Add tests for bitmask_to_list and get_cpu_affinity_list

--- dask_cuda/tests/test_utils.py
+++ dask_cuda/tests/test_utils.py
@@ -1,6 +1,8 @@
+import pytest
+
 import os
 
-from dask_cuda.utils import get_n_gpus
+from dask_cuda.utils import bitmask_to_list, get_cpu_affinity_list, get_n_gpus
 
 
 def test_get_n_gpus():
@@ -11,3 +13,36 @@ def test_get_n_gpus():
         assert get_n_gpus() == 3
     finally:
         del os.environ["CUDA_VISIBLE_DEVICES"]
+
+
+@pytest.mark.parametrize(
+    "params",
+    [
+        {
+            "input": [1152920405096267775, 0],
+            "output": [i for i in range(20)] + [i + 40 for i in range(20)],
+        },
+        {
+            "input": [17293823668613283840, 65535],
+            "output": [i + 20 for i in range(20)] + [i + 60 for i in range(20)],
+        },
+        {
+            "input": [18446744073709551615, 0],
+            "output": [i for i in range(64)],
+        },
+        {
+            "input": [0, 18446744073709551615],
+            "output": [i + 64 for i in range(64)],
+        },
+    ],
+)
+def test_bitmask_to_list(params):
+    assert bitmask_to_list(params["input"]) == params["output"]
+
+
+
+def test_cpu_affinity():
+    for i in range(get_n_gpus()):
+        affinity = get_cpu_affinity_list(i)
+        os.sched_setaffinity(0, affinity)
+        assert list(os.sched_getaffinity(0)) == affinity
