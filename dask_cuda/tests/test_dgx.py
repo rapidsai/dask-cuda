41e27f25e06eda8c21ed09fa1c29ee84f58fe95d
Peter Andreas Entschev <peter@entschev.com>
2019-10-07 05:58:26 -0700

    Fix TCP over UCX test

--- dask_cuda/tests/test_dgx.py
+++ dask_cuda/tests/test_dgx.py
@@ -27,25 +27,25 @@ async def test_dgx():
 )
 @gen_test(timeout=20)
 async def test_dgx_tcp_over_ucx():
-    ucp = pytest.importorskip("ucp")
-    with pytest.warns(UserWarning):
-        ucx_env = get_ucx_env(enable_infiniband=True, enable_nvlink=True)
-        os.environ.update(ucx_env)
+    ucx_env = get_ucx_env(enable_tcp=True)
+    os.environ.update(ucx_env)
 
-        async with DGX(
-            protocol="ucx",
-            enable_tcp_over_ucx=True,
-            asynchronous=True,
-        ) as cluster:
-            async with Client(cluster, asynchronous=True):
-                pass
+    ucp = pytest.importorskip("ucp")
+
+    async with DGX(
+        protocol="ucx",
+        enable_tcp_over_ucx=True,
+        asynchronous=True,
+    ) as cluster:
+        async with Client(cluster, asynchronous=True):
+            pass
 
 
 @pytest.mark.skipif(
     "ib0" not in psutil.net_if_addrs(), reason="Infiniband interface ib0 not found"
 )
 @gen_test(timeout=20)
-async def test_dgx_ucx():
+async def test_dgx_ucx_infiniband_nvlink():
     ucp = pytest.importorskip("ucp")
     with pytest.warns(UserWarning):
         ucx_env = get_ucx_env(enable_infiniband=True, enable_nvlink=True)
