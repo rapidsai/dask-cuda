9c0bf1bea5faf5cc4185a37caeacc958f9924408
Mads R. B. Kristensen <madsbk@gmail.com>
2019-11-11 20:15:48 +0100

    Updated CI to always include the newest packages

--- ci/gpu/build.sh
+++ ci/gpu/build.sh
@@ -1,4 +1,8 @@
-#!/usr/bin/env bash
+#!/bin/bash
+# Copyright (c) 2018, NVIDIA CORPORATION.
+##############################################
+# dask-cuda GPU build and test script for CI #
+##############################################
 set -e
 NUMARGS=$#
 ARGS=$*
@@ -15,17 +19,18 @@ function hasArg {
 
 # Set path and build parallel level
 export PATH=/conda/bin:/usr/local/cuda/bin:$PATH
-export CUDA_REL=${CUDA//./}
+export PARALLEL_LEVEL=4
+export CUDA_REL=${CUDA_VERSION%.*}
+export CUDA_REL2=${CUDA//./}
 
 # Set home to the job's workspace
 export HOME=$WORKSPACE
 
-# Switch to project root; also root of repo checkout
-cd $WORKSPACE
-
 # Parse git describe
+cd $WORKSPACE
 export GIT_DESCRIBE_TAG=`git describe --tags`
 export MINOR_VERSION=`echo $GIT_DESCRIBE_TAG | grep -o -E '([0-9]+\.[0-9]+)'`
+export UCX_PATH=$CONDA_PREFIX
 
 # Enable NumPy's __array_function__ protocol (needed for NumPy 1.16.x,
 # will possibly be enabled by default starting on 1.17)
@@ -35,42 +40,80 @@ export NUMPY_EXPERIMENTAL_ARRAY_FUNCTION=1
 # SETUP - Check environment
 ################################################################################
 
-logger "Get env..."
+logger "Check environment..."
 env
+
+logger "Check GPU usage..."
+nvidia-smi
 
 logger "Activate conda env..."
 source activate gdf
+conda list
+
+# Fixing Numpy version to avoid RuntimeWarning: numpy.ufunc size changed, may
+# indicate binary incompatibility. Expected 192 from C header, got 216 from PyObject
+conda install "cudatoolkit=$CUDA_REL" \
+              "cupy>=6.5.0" "numpy=1.16.4" \
+              "cudf>=0.10" "dask-cudf>=0.10" \
+              "dask>=2.3.0" "distributed>=2.3.2" \
+              -c rapidsai-nightly
+
+# needed for asynccontextmanager in py36
+conda install -c conda-forge "async_generator" "automake" "libtool" \
+                              "cmake" "automake" "autoconf" "cython" \
+                              "pytest" "pkg-config" "pytest-asyncio"
+conda list
+
+# Install the master version of dask and distributed
+logger "pip install git+https://github.com/dask/distributed.git --upgrade"
+pip install "git+https://github.com/dask/distributed.git" --upgrade
+logger "pip install git+https://github.com/dask/dask.git --upgrade"
+pip install "git+https://github.com/dask/dask.git" --upgrade
 
 logger "Check versions..."
 python --version
-gcc --version
-g++ --version
-
-# FIX Added to deal with Anancoda SSL verification issues during conda builds
-conda config --set ssl_verify False
-
-conda install \
-    "cudf=$MINOR_VERSION.*" \
-    "dask-cudf=$MINOR_VERSION.*"
+$CC --version
+$CXX --version
+conda list
 
 ################################################################################
-# SETUP - Install additional packages
+# BUILD - Build ucx
+################################################################################
+
+logger "Build ucx"
+git clone https://github.com/openucx/ucx
+cd ucx
+ls
+./autogen.sh
+mkdir build
+cd build
+../configure --prefix=$CONDA_PREFIX --enable-debug --with-cuda=$CUDA_HOME --enable-mt CPPFLAGS="-I//$CUDA_HOME/include"
+make -j install
+cd $WORKSPACE
+
+
+################################################################################
+# BUILD - Build ucx-py
+################################################################################
+
+logger "Build dask-cuda..."
+cd $WORKSPACE
+python -m pip install -e .
+
+
+################################################################################
+# TEST - Run py.tests for ucx-py
 ################################################################################
 
 if hasArg --skip-tests; then
     logger "Skipping Tests..."
 else
-    # Install CuPy for tests
-    pip install cupy-cuda${CUDA_REL}==6.0.0
+    logger "Check NICs"
+    awk 'END{print $1}' /etc/hosts
+    cat /etc/hosts
 
-    # Install distributed@master (temporarily required due to issues with 2.3.2)
-    # pip install git+https://github.com/dask/distributed.git@master
+    logger "Python py.test for dask-cuda..."
+    cd $WORKSPACE
 
-    ################################################################################
-    # TEST - Run tests
-    ################################################################################
-
-    pip install -e .
-    pip install pytest pytest-asyncio fsspec
-    py.test --cache-clear --junitxml=${WORKSPACE}/junit-dask-cuda.xml -v --cov-config=.coveragerc --cov=dask_cuda --cov-report=xml:${WORKSPACE}/dask-cuda-coverage.xml --cov-report term
+    UCXPY_IFNAME=eth0 UCX_WARN_UNUSED_ENV_VARS=n UCX_MEMTYPE_CACHE=n py.test --cache-clear --junitxml=${WORKSPACE}/junit-dask-cuda.xml -v --cov-config=.coveragerc --cov=dask_cuda --cov-report=xml:${WORKSPACE}/dask-cuda-coverage.xml --cov-report term dask_cuda/tests/
 fi
