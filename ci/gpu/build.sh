11b7220f604787ce2918cc0e3cd5b4cc8ca44f80
Dillon Cullinan <dcullinan@dgx01.aselab.nvidia.com>
2019-11-13 08:28:49 -0800

    Fix gpu dep versions

--- ci/gpu/build.sh
+++ ci/gpu/build.sh
@@ -54,7 +54,7 @@ conda list
 # indicate binary incompatibility. Expected 192 from C header, got 216 from PyObject
 conda install "cudatoolkit=$CUDA_REL" \
               "cupy>=6.5.0" "numpy=1.16.4" \
-              "cudf>=0.10" "dask-cudf>=0.10" \
+              "cudf>=${MINOR_VERSION}" "dask-cudf>=${MINOR_VERSION}" \
               "dask>=2.3.0" "distributed>=2.3.2" \
               -c rapidsai-nightly
 
