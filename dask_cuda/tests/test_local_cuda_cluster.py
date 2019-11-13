8a02d167bee9bb8666f26dcf2d0c990a68605daa
Mads R. B. Kristensen <madsbk@gmail.com>
2019-11-13 11:15:42 +0100

    test_ucx_protocol: removed the xfail mark

--- dask_cuda/tests/test_local_cuda_cluster.py
+++ dask_cuda/tests/test_local_cuda_cluster.py
@@ -71,7 +71,6 @@ async def test_with_subset_of_cuda_visible_devices():
         del os.environ["CUDA_VISIBLE_DEVICES"]
 
 
-@pytest.mark.xfail(reason="https://github.com/rapidsai/dask-cuda/pull/168")
 @gen_test(timeout=20)
 async def test_ucx_protocol():
     pytest.importorskip("distributed.comm.ucx")
