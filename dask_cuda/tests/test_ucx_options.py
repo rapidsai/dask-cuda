3a5e8619977fb0ccdf65237cf42e67021fb58805
Mads R. B. Kristensen <madsbk@gmail.com>
2019-11-13 10:02:31 +0100

    Added UCX option tests

---
+++ dask_cuda/tests/test_ucx_options.py
@@ -0,0 +1,38 @@
+import os
+import multiprocessing as mp
+import numpy
+import pytest
+import dask.array as da
+import dask
+from distributed.deploy.local import LocalCluster
+from distributed import Client
+
+mp = mp.get_context("spawn")
+ucp = pytest.importorskip("ucp")
+
+
+def _test_global_option(seg_size):
+    """Test setting UCX options through dask's global config"""
+    dask.config.set({"ucx": {"SEG_SIZE": seg_size}})
+
+    with LocalCluster(
+        protocol="ucx",
+        dashboard_address=None,
+        n_workers=1,
+        threads_per_worker=1,
+        processes=True,
+    ) as cluster:
+        with Client(cluster):
+            res = da.from_array(numpy.arange(10000), chunks=(1000,))
+            res = res.sum().compute()
+            assert res == 49995000
+            conf = ucp.get_config()
+            assert conf["SEG_SIZE"] == seg_size
+
+
+def test_global_option():
+    for seg_size in ["2K", "1M", "2M"]:
+        p = mp.Process(target=_test_global_option, args=(seg_size,))
+        p.start()
+        p.join()
+        assert not p.exitcode
