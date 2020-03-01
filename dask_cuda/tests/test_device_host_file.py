4cb86189855392854f371264c272a661d1d6ec97
John Kirkham <jakirkham@gmail.com>
2020-03-01 01:53:21 -0800

    Test `DeviceHostFile` uses `temporary-directory`
    Make sure that if `local_directory` is not specified to
    `DeviceHostFile`, that Dask's config value `temporary-directory` is used
    instead.

--- dask_cuda/tests/test_device_host_file.py
+++ dask_cuda/tests/test_device_host_file.py
@@ -1,5 +1,6 @@
 from random import randint
 
+import dask
 import dask.array as da
 from dask_cuda.device_host_file import (
     DeviceHostFile,
@@ -12,6 +13,13 @@ import numpy as np
 import pytest
 
 cupy = pytest.importorskip("cupy")
+
+
+def test_device_host_file_config(tmp_path):
+    dhf_disk_path = str(tmp_path / "dask-worker-space" / "storage")
+    with dask.config.set(temporary_directory=str(tmp_path)):
+        dhf = DeviceHostFile()
+        assert dhf.disk_func.d.directory == dhf_disk_path
 
 
 @pytest.mark.parametrize("num_host_arrays", [1, 10, 100])
