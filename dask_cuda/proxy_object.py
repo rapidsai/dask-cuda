349a3939f2a3ba9247f85c663d75308ef58209b7
Mads R. B. Kristensen <madsbk@gmail.com>
2020-08-07 05:36:14 -0700

    Added "*args, **kwargs" to dispatch of ObjectProxy

--- dask_cuda/proxy_object.py
+++ dask_cuda/proxy_object.py
@@ -160,9 +160,13 @@ def obj_pxy_dask_deserialize(header, frames):
 
 @dask.dataframe.utils.hash_object_dispatch.register(ObjectProxy)
 def obj_pxy_hash_object(obj: ObjectProxy, *args, **kwargs):
-    return dask.dataframe.utils.hash_object_dispatch(obj._obj_pxy_deserialize())
+    return dask.dataframe.utils.hash_object_dispatch(
+        obj._obj_pxy_deserialize(), *args, **kwargs
+    )
 
 
 @dask.dataframe.utils.group_split_dispatch.register(ObjectProxy)
 def obj_pxy_group_split(obj: ObjectProxy, *args, **kwargs):
-    return dask.dataframe.utils.hash_object_dispatch(obj._obj_pxy_deserialize())
+    return dask.dataframe.utils.hash_object_dispatch(
+        obj._obj_pxy_deserialize(), *args, **kwargs
+    )
