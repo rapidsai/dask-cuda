373dc4006e88a66225bf53b1e1f8764b40ffec5f
Peter Andreas Entschev <peter@entschev.com>
2020-10-07 14:08:40 -0700

    Skip cuDF explicit_comms tests when not installed

--- dask_cuda/tests/test_explicit_comms.py
+++ dask_cuda/tests/test_explicit_comms.py
@@ -10,9 +10,6 @@ from dask.dataframe.shuffle import partitioning_index
 from distributed import Client
 from distributed.deploy.local import LocalCluster
 
-import cudf
-from cudf.tests.utils import assert_eq
-
 from dask_cuda.explicit_comms import (
     CommsContext,
     dataframe_merge,
@@ -52,6 +49,12 @@ def test_local_cluster(protocol):
 
 
 def _test_dataframe_merge(backend, protocol, n_workers):
+    if backend == "cudf":
+        cudf = pytest.importorskip("cudf")
+        from cudf.tests.utils import assert_eq
+    else:
+        from dask.dataframe.utils import assert_eq
+
     dask.config.update(
         dask.config.global_config,
         {
@@ -103,6 +106,8 @@ def _test_dataframe_merge(backend, protocol, n_workers):
 @pytest.mark.parametrize("backend", ["pandas", "cudf"])
 @pytest.mark.parametrize("protocol", ["tcp", "ucx"])
 def test_dataframe_merge(backend, protocol, nworkers):
+    if backend == "cudf":
+        cudf = pytest.importorskip("cudf")
     p = mp.Process(target=_test_dataframe_merge, args=(backend, protocol, nworkers))
     p.start()
     p.join()
@@ -148,6 +153,12 @@ def check_partitions(df, npartitions):
 
 
 def _test_dataframe_shuffle(backend, protocol, n_workers):
+    if backend == "cudf":
+        cudf = pytest.importorskip("cudf")
+        from cudf.tests.utils import assert_eq
+    else:
+        from dask.dataframe.utils import assert_eq
+
     dask.config.update(
         dask.config.global_config,
         {
@@ -184,6 +195,9 @@ def _test_dataframe_shuffle(backend, protocol, n_workers):
 @pytest.mark.parametrize("backend", ["pandas", "cudf"])
 @pytest.mark.parametrize("protocol", ["tcp", "ucx"])
 def test_dataframe_shuffle(backend, protocol, nworkers):
+    if backend == "cudf":
+        cudf = pytest.importorskip("cudf")
+
     p = mp.Process(target=_test_dataframe_shuffle, args=(backend, protocol, nworkers))
     p.start()
     p.join()
