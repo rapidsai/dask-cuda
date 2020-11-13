c36a65843a592d36f256f8451b91b20f8320cb1d
Mark Sadang <marksadang@engineer.com>
2020-11-12 20:59:42 -0800

    Updated upload.sh and build.sh

--- ci/cpu/build.sh
+++ ci/cpu/build.sh
@@ -1,55 +1,26 @@
 #!/usr/bin/env bash
-# Copyright (c) 2020, NVIDIA CORPORATION.
+# Copyright (c) 2019, NVIDIA CORPORATION.
 ################################################################################
 # dask-cuda cpu build
 ################################################################################
 set -e
 
-# Logger function for build status output
-function gpuci_logger() {
-  echo -e "\n>>>> $@\n"
-}
-
 # Set path and build parallel level
 export PATH=/opt/conda/bin:/usr/local/cuda/bin:$PATH
+export PARALLEL_LEVEL=${PARALLEL_LEVEL:-4}
 
 # Set home to the job's workspace
 export HOME=$WORKSPACE
 
-# Switch to project root; also root of repo checkout
-cd $WORKSPACE
-
-# Get latest tag and number of commits since tag
-export GIT_DESCRIBE_TAG=`git describe --abbrev=0 --tags`
-export GIT_DESCRIBE_NUMBER=`git rev-list ${GIT_DESCRIBE_TAG}..HEAD --count`
+# Determine CUDA release version
+export CUDA_REL=${CUDA_VERSION%.*}
 
 # Setup 'gpuci_conda_retry' for build retries (results in 2 total attempts)
 export GPUCI_CONDA_RETRY_MAX=1
 export GPUCI_CONDA_RETRY_SLEEP=30
 
-################################################################################
-# SETUP - Check environment
-################################################################################
-
-gpuci_logger "Get env"
-env
-
-gpuci_logger "Activate conda env"
-. /opt/conda/etc/profile.d/conda.sh
-conda activate rapids
-
-gpuci_logger "Check versions"
-python --version
-$CC --version
-$CXX --version
-conda info
-conda config --show-sources
-conda list --show-channel-urls
-
-# FIX Added to deal with Anancoda SSL verification issues during conda builds
-conda config --set ssl_verify False
-
-pip install git+https://github.com/dask/distributed.git@master
+# Switch to project root; also root of repo checkout
+cd $WORKSPACE
 
 # If nightly build, append current YYMMDD to version
 if [[ "$BUILD_MODE" = "branch" && "$SOURCE_BRANCH" = branch-* ]] ; then
@@ -57,9 +28,34 @@ if [[ "$BUILD_MODE" = "branch" && "$SOURCE_BRANCH" = branch-* ]] ; then
 fi
 
 ################################################################################
+# SETUP - Check environment
+################################################################################
+
+gpuci_logger "Check environment variables"
+env
+
+gpuci_logger "Activate conda env"
+. /opt/conda/etc/profile.d/conda.sh
+conda activate rapids
+
+gpuci_logger "Check compiler versions"
+python --version
+$CC --version
+$CXX --version
+
+gpuci_logger "Check conda environment"
+conda info
+conda config --show-sources
+conda list --show-channel-urls
+
+# FIX Added to deal with Anancoda SSL verification issues during conda builds
+conda config --set ssl_verify False
+
+################################################################################
 # BUILD - Package builds
 ################################################################################
 
+gpuci_logger "Build conda pkg for libcudf"
 gpuci_conda_retry build conda/recipes/dask-cuda --python=${PYTHON}
 
 rm -rf dist/
@@ -69,8 +65,8 @@ python setup.py sdist bdist_wheel
 # UPLOAD - Packages
 ################################################################################
 
-gpuci_logger "Upload conda pkg"
-source ci/cpu/upload-anaconda.sh
+gpuci_logger "Upload conda pkg..."
+source ci/cpu/upload.sh
 
-gpuci_logger "Upload pypi pkg"
+gpuci_logger "Upload pypi pkg..."
 source ci/cpu/upload-pypi.sh
