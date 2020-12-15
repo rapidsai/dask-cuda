6b37c27fcd9021a86fc6f454f23d9f372065e61b
Mads R. B. Kristensen <madsbk@gmail.com>
2020-12-15 17:11:08 +0100

    Implemented UnspilledProxies

--- dask_cuda/object_spilling_host_file.py
+++ dask_cuda/object_spilling_host_file.py
@@ -18,13 +18,42 @@ from .proxify_device_object import proxify_device_object
 from .proxy_object import ProxyObject
 
 
+class UnspilledProxies:
+    def __init__(self):
+        self.dev_mem_usage = 0
+        self.proxy_id_to_dev_mems: DefaultDict[int, Set[Hashable]] = defaultdict(set)
+        self.dev_mem_to_proxy_ids: DefaultDict[Hashable, Set[int]] = defaultdict(set)
+
+    def add(self, proxy: ProxyObject):
+        proxy_id = id(proxy)
+        if proxy_id not in self.proxy_id_to_dev_mems:
+            for dev_mem in proxy._obj_pxy_get_device_memory_objects():
+                self.proxy_id_to_dev_mems[proxy_id].add(dev_mem)
+                ps = self.dev_mem_to_proxy_ids[dev_mem]
+                if len(ps) == 0:
+                    self.dev_mem_usage += sizeof(dev_mem)
+                ps.add(proxy_id)
+
+    def remove(self, proxy: ProxyObject):
+        proxy_id = id(proxy)
+        if proxy_id in self.proxy_id_to_dev_mems:
+            for dev_mem in self.proxy_id_to_dev_mems.pop(proxy_id):
+                self.dev_mem_to_proxy_ids[dev_mem].remove(proxy_id)
+                if len(self.dev_mem_to_proxy_ids[dev_mem]) == 0:
+                    del self.dev_mem_to_proxy_ids[dev_mem]
+                    self.dev_mem_usage -= sizeof(dev_mem)
+
+    def __iter__(self):
+        return iter(self.proxy_id_to_dev_mems)
+
+
 class ProxiesTally:
     def __init__(self):
         self.lock = threading.RLock()
         self.proxy_id_to_proxy: Dict[int, ProxyObject] = {}
         self.key_to_proxy_ids: DefaultDict[Hashable, Set[int]] = defaultdict(set)
         self.proxy_id_to_keys: DefaultDict[int, Set[Hashable]] = defaultdict(set)
-        self.unspilled_proxy_ids: Set = set()
+        self.unspilled_proxies = UnspilledProxies()
 
     def add_key(self, key, proxies: List[ProxyObject]):
         with self.lock:
@@ -34,7 +63,7 @@ class ProxiesTally:
                 self.key_to_proxy_ids[key].add(proxy_id)
                 self.proxy_id_to_keys[proxy_id].add(key)
                 if not proxy._obj_pxy_serialized():
-                    self.unspilled_proxy_ids.add(proxy_id)
+                    self.unspilled_proxies.add(proxy)
 
     def del_key(self, key):
         with self.lock:
@@ -42,29 +71,25 @@ class ProxiesTally:
                 self.proxy_id_to_keys[proxy_id].remove(key)
                 if len(self.proxy_id_to_keys[proxy_id]) == 0:
                     del self.proxy_id_to_keys[proxy_id]
-                    del self.proxy_id_to_proxy[proxy_id]
-                    self.unspilled_proxy_ids.discard(proxy_id)
+                    self.unspilled_proxies.remove(self.proxy_id_to_proxy.pop(proxy_id))
 
     def spill_proxy(self, proxy: ProxyObject):
         with self.lock:
-            proxy_id = id(proxy)
-            assert proxy_id in self.proxy_id_to_proxy
-            assert proxy_id in self.unspilled_proxy_ids
-            self.unspilled_proxy_ids.remove(proxy_id)
+            self.unspilled_proxies.remove(proxy)
 
     def unspill_proxy(self, proxy: ProxyObject):
         with self.lock:
-            proxy_id = id(proxy)
-            assert proxy_id in self.proxy_id_to_proxy
-            assert proxy_id not in self.unspilled_proxy_ids
-            self.unspilled_proxy_ids.add(proxy_id)
+            self.unspilled_proxies.add(proxy)
 
     def get_unspilled_proxies(self):
         with self.lock:
-            for proxy_id in self.unspilled_proxy_ids:
+            for proxy_id in self.unspilled_proxies:
                 ret = self.proxy_id_to_proxy[proxy_id]
                 assert not ret._obj_pxy_serialized()
                 yield ret
+
+    def get_dev_mem_usage(self):
+        return self.unspilled_proxies.dev_mem_usage
 
 
 class ObjectSpillingHostFile(MutableMapping):
@@ -122,23 +147,25 @@ class ObjectSpillingHostFile(MutableMapping):
             return ret
 
     def get_dev_buffer_to_proxies(self) -> DefaultDict[Hashable, List[ProxyObject]]:
-        # Notice, multiple proxy object can point to different non-overlapping
-        # parts of the same device buffer.
-        ret = DefaultDict(list)
-        for proxy in self.get_proxied_id_to_proxy().values():
-            for dev_buffer in proxy._obj_pxy_get_device_memory_objects():
-                ret[dev_buffer].append(proxy)
-        return ret
+        with self.lock:
+            # Notice, multiple proxy object can point to different non-overlapping
+            # parts of the same device buffer.
+            ret = DefaultDict(list)
+            for proxy in self.get_proxied_id_to_proxy().values():
+                for dev_buffer in proxy._obj_pxy_get_device_memory_objects():
+                    ret[dev_buffer].append(proxy)
+            return ret
 
     def get_access_info(self) -> Tuple[int, List[Tuple[int, int, List[ProxyObject]]]]:
-        total_dev_mem_usage = 0
-        dev_buf_access = []
-        for dev_buf, proxies in self.get_dev_buffer_to_proxies().items():
-            last_access = max(p._obj_pxy.get("last_access", 0) for p in proxies)
-            size = sizeof(dev_buf)
-            dev_buf_access.append((last_access, size, proxies))
-            total_dev_mem_usage += size
-        return total_dev_mem_usage, dev_buf_access
+        with self.lock:
+            total_dev_mem_usage = 0
+            dev_buf_access = []
+            for dev_buf, proxies in self.get_dev_buffer_to_proxies().items():
+                last_access = max(p._obj_pxy.get("last_access", 0) for p in proxies)
+                size = sizeof(dev_buf)
+                dev_buf_access.append((last_access, size, proxies))
+                total_dev_mem_usage += size
+            return total_dev_mem_usage, dev_buf_access
 
     def __setitem__(self, key, value):
         with self.lock:
@@ -168,6 +195,12 @@ class ObjectSpillingHostFile(MutableMapping):
         proxy._obj_pxy_serialize(serializers=("dask", "pickle"))
 
     def maybe_evict(self, extra_dev_mem=0):
+        if ( # Shortcut when not evicting
+            self.proxies_tally.get_dev_mem_usage() + extra_dev_mem
+            <= self.device_memory_limit
+        ):
+            return
+
         total_dev_mem_usage, dev_buf_access = self.get_access_info()
         total_dev_mem_usage += extra_dev_mem
         if total_dev_mem_usage > self.device_memory_limit:
