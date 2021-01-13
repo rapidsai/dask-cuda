d6517f5a4778e09ee308e5d7c11224707c64b05d
Mads R. B. Kristensen <madsbk@gmail.com>
2021-01-13 14:59:13 +0100

    Making dataframe_merge.py self-sustained

--- dask_cuda/explicit_comms/dataframe_merge.py
+++ dask_cuda/explicit_comms/dataframe_merge.py
@@ -1,13 +1,62 @@
 import asyncio
 
+from dask.dataframe.core import _concat
+from distributed.protocol import to_serialize
+
 from . import comms
-from .dataframe_shuffle import (
-    df_concat,
-    exchange_and_concat_bins,
-    partition_by_hash,
-    recv_df,
-    send_df,
-)
+from .dataframe_shuffle import partition_by_hash
+
+
+async def send_df(ep, df):
+    if df is None:
+        return await ep.write("empty")
+    else:
+        return await ep.write([to_serialize(df)])
+
+
+async def recv_df(ep):
+    ret = await ep.read()
+    if ret == "empty":
+        return None
+    else:
+        return ret[0]
+
+
+async def barrier(rank, eps):
+    if rank == 0:
+        await asyncio.gather(*[ep.read() for ep in eps.values()])
+    else:
+        await eps[0].write("dummy")
+
+
+async def send_bins(eps, bins):
+    futures = []
+    for rank, ep in eps.items():
+        futures.append(send_df(ep, bins[rank]))
+    await asyncio.gather(*futures)
+
+
+async def recv_bins(eps, bins):
+    futures = []
+    for ep in eps.values():
+        futures.append(recv_df(ep))
+    bins.extend(await asyncio.gather(*futures))
+
+
+async def exchange_and_concat_bins(rank, eps, bins):
+    ret = [bins[rank]]
+    await asyncio.gather(recv_bins(eps, ret), send_bins(eps, bins))
+    return _concat([df for df in ret if df is not None])
+
+
+def df_concat(df_parts):
+    """Making sure df_parts is a single dataframe or None"""
+    if len(df_parts) == 0:
+        return None
+    elif len(df_parts) == 1:
+        return df_parts[0]
+    else:
+        return _concat(df_parts)
 
 
 async def broadcast(rank, root_rank, eps, df=None):
