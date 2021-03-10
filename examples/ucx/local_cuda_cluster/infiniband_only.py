38cd9fe476864310463cd6f17266378e58a53706
Charles Blackmon-Luca <20627856+charlesbluca@users.noreply.github.com>
2021-03-10 12:49:37 -0800

    Add basic UCX examples

---
+++ examples/ucx/local_cuda_cluster/infiniband_only.py
@@ -0,0 +1,17 @@
+from dask.distributed import Client
+from dask_cuda import LocalCUDACluster
+from dask_cuda.initialize import initialize
+
+
+def main():
+    cluster = LocalCUDACluster(
+        protocol="ucx",
+        interface="ib0", # passed to the scheduler
+        enable_tcp_over_ucx=True,
+        enable_infiniband=True,
+        ucx_net_devices="auto",
+        rmm_pool_size="1GB",
+    )
+
+if __name__ == "__main__":
+    main()
