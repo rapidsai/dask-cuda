b882bf2456369e056f8953ba1497634ba977008e
Charles Blackmon-Luca <20627856+charlesbluca@users.noreply.github.com>
2021-03-17 19:18:45 -0400

    Add standalone examples of UCX usage (#551)
    Following up on #545, this PR adds standalone examples of UCX usage to a new `examples/ucx/` directory.
    
    Right now, these are pretty simple - just showing cluster, worker, and client setups for NVLink, InfiniBand, both, or neither. I imagine there are problem some more complicated/niche set up we could showcase here.
    
    cc @pentschev
    
    Authors:
      - Charles Blackmon-Luca (@charlesbluca)
    
    Approvers:
      - Peter Andreas Entschev (@pentschev)
    
    URL: https://github.com/rapidsai/dask-cuda/pull/551

---
+++ examples/ucx/client_initialize.py
@@ -0,0 +1,47 @@
+import click
+
+from dask.distributed import Client
+
+from dask_cuda.initialize import initialize
+
+
+@click.command(context_settings=dict(ignore_unknown_options=True))
+@click.argument(
+    "address", required=True, type=str,
+)
+@click.option(
+    "--enable-nvlink/--disable-nvlink",
+    default=False,
+    help="Enable NVLink communication",
+)
+@click.option(
+    "--enable-infiniband/--disable-infiniband",
+    default=False,
+    help="Enable InfiniBand communication with RDMA",
+)
+def main(
+    address, enable_nvlink, enable_infiniband,
+):
+
+    enable_rdmacm = False
+    ucx_net_devices = None
+
+    if enable_infiniband:
+        enable_rdmacm = True
+        ucx_net_devices = "mlx5_0:1"
+
+    # set up environment
+    initialize(
+        enable_tcp_over_ucx=True,
+        enable_nvlink=enable_nvlink,
+        enable_infiniband=enable_infiniband,
+        enable_rdmacm=enable_rdmacm,
+        net_devices=ucx_net_devices,
+    )
+
+    # initialize client
+    client = Client(address)  # noqa F841
+
+
+if __name__ == "__main__":
+    main()
---
+++ examples/ucx/local_cuda_cluster.py
@@ -0,0 +1,67 @@
+import click
+
+from dask.distributed import Client
+from dask.utils import parse_bytes
+
+from dask_cuda import LocalCUDACluster
+
+
+@click.command(context_settings=dict(ignore_unknown_options=True))
+@click.option(
+    "--enable-nvlink/--disable-nvlink",
+    default=False,
+    help="Enable NVLink communication",
+)
+@click.option(
+    "--enable-infiniband/--disable-infiniband",
+    default=False,
+    help="Enable InfiniBand communication with RDMA",
+)
+@click.option(
+    "--interface",
+    default=None,
+    type=str,
+    help="Interface used by scheduler for communication. Must be "
+    "specified if NVLink or InfiniBand are enabled.",
+)
+@click.option(
+    "--rmm-pool-size",
+    default="1GB",
+    type=parse_bytes,
+    help="If specified, initialize each worker with an RMM pool of "
+    "the given size, otherwise no RMM pool is created. This can be "
+    "an integer (bytes) or string (like 5GB or 5000M).",
+)
+def main(
+    enable_nvlink, enable_infiniband, interface, rmm_pool_size,
+):
+
+    enable_rdmacm = False
+    ucx_net_devices = None
+
+    if enable_infiniband:
+        enable_rdmacm = True
+        ucx_net_devices = "auto"
+
+    if (enable_infiniband or enable_nvlink) and not interface:
+        raise ValueError(
+            "Interface must be specified if NVLink or Infiniband are enabled"
+        )
+
+    # initialize scheduler & workers
+    cluster = LocalCUDACluster(
+        enable_tcp_over_ucx=True,
+        enable_nvlink=enable_nvlink,
+        enable_infiniband=enable_infiniband,
+        enable_rdmacm=enable_rdmacm,
+        ucx_net_devices=ucx_net_devices,
+        interface=interface,
+        rmm_pool_size=rmm_pool_size,
+    )
+
+    # initialize client
+    client = Client(cluster)  # noqa F841
+
+
+if __name__ == "__main__":
+    main()
