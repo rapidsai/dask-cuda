da68de701472550eb455a38696f9f4337e71bb12
jakirkham <jakirkham@gmail.com>
2021-04-16 14:59:41 -0700

    Clarify Python requirements (#576)
    * Drop Python 3.6 trove classifier
    * Add Python 3.8 trove classifier
    * Add general Python 3 trove classifier
    * Include `python_requires` to clarify Python 3.7+ version requirement
    
    Authors:
      - https://github.com/jakirkham
    
    Approvers:
      - Peter Andreas Entschev (https://github.com/pentschev)
    
    URL: https://github.com/rapidsai/dask-cuda/pull/576

--- setup.py
+++ setup.py
@@ -32,10 +32,12 @@ setup(
         "Topic :: Database",
         "Topic :: Scientific/Engineering",
         "License :: OSI Approved :: Apache Software License",
-        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     packages=find_packages(exclude=["docs", "tests", "tests.*", "docs.*"]),
+    python_requires=">=3.7",
     install_requires=open("requirements.txt").read().strip().split("\n"),
     entry_points="""
         [console_scripts]
