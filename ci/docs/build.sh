3c3605ed1b8e8655e69e08c5dcff7962daa56b04
Charles Blackmon-Luca <20627856+charlesbluca@users.noreply.github.com>
2021-04-22 11:21:19 -0400

    Add docs CI build script (#580)
    Adds a docs build script for gpuCI to execute on merges; addresses #579.
    
    Depends on rapidsai/integration#250
    
    cc @quasiben
    
    Authors:
      - Charles Blackmon-Luca (https://github.com/charlesbluca)
    
    Approvers:
      - Dillon Cullinan (https://github.com/dillon-cullinan)
    
    URL: https://github.com/rapidsai/dask-cuda/pull/580

---
+++ ci/docs/build.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+# Copyright (c) 2021, NVIDIA CORPORATION.
+######################################
+# Dask-CUDA Docs build script for CI #
+######################################
+
+if [ -z "$PROJECT_WORKSPACE" ]; then
+    echo ">>>> ERROR: Could not detect PROJECT_WORKSPACE in environment"
+    echo ">>>> WARNING: This script contains git commands meant for automated building, do not run locally"
+    exit 1
+fi
+
+export DOCS_WORKSPACE=$WORKSPACE/docs
+export PATH=/conda/bin:/usr/local/cuda/bin:$PATH
+export HOME=$WORKSPACE
+export PROJECT_WORKSPACE=/rapids/dask-cuda
+export NIGHTLY_VERSION=$(echo $BRANCH_VERSION | awk -F. '{print $2}')
+
+gpuci_logger "Check environment..."
+env
+
+gpuci_logger "Check GPU usage..."
+nvidia-smi
+
+gpuci_logger "Activate conda env..."
+. /opt/conda/etc/profile.d/conda.sh
+conda activate rapids
+
+gpuci_logger "Check versions..."
+python --version
+$CC --version
+$CXX --version
+conda info
+conda config --show-sources
+conda list --show-channel-urls
+
+# Dask-CUDA Sphinx build
+gpuci_logger "Build Dask-CUDA docs..."
+cd $PROJECT_WORKSPACE/docs
+make html
+
+# commit to website
+cd $DOCS_WORKSPACE
+
+if [ ! -d "api/dask-cuda/$BRANCH_VERSION" ]; then
+    mkdir -p api/dask-cuda/$BRANCH_VERSION
+fi
+rm -rf $DOCS_WORKSPACE/api/dask-cuda/$BRANCH_VERSION/*
+
+mv $PROJECT_WORKSPACE/docs/build/html/* $DOCS_WORKSPACE/api/dask-cuda/$BRANCH_VERSION
