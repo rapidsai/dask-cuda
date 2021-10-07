f3d7604892d1049093f34f02ae9632e2cee85a8d
Mads R. B. Kristensen <madsbk@gmail.com>
2021-10-07 15:09:23 +0200

    Removing FrameProxyObject
    Now that cuDF doesn't use the cython implemented Table class,
    we don't need the specialized FrameProxyObject class anymore.
    We can simpy use ProxyObject everywhere.

--- dask_cuda/tests/test_proxify_host_file.py
+++ dask_cuda/tests/test_proxify_host_file.py
@@ -181,7 +181,7 @@ def test_local_cuda_cluster(jit_unspill):
         assert isinstance(x, cudf.DataFrame)
         if jit_unspill:
             # Check that `x` is a proxy object and the proxied DataFrame is serialized
-            assert "FrameProxyObject" in str(type(x))
+            assert "ProxyObject" in str(type(x))
             assert x._obj_pxy["serializer"] == "dask"
         else:
             assert type(x) == cudf.DataFrame
