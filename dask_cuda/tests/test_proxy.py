18cad7883e7ef6045b4cd775b412009671773bd4
Mads R. B. Kristensen <madsbk@gmail.com>
2021-10-11 09:42:47 +0200

    Temporarily skipping some tests because of a bug in Dask (#753)
    See <https://github.com/rapidsai/dask-cuda/issues/746>
    
    Will submit a PR that revert this when https://github.com/rapidsai/dask-cuda/issues/746 has been merged.
    
    Authors:
      - Mads R. B. Kristensen (https://github.com/madsbk)
    
    Approvers:
      - Peter Andreas Entschev (https://github.com/pentschev)
    
    URL: https://github.com/rapidsai/dask-cuda/pull/753

--- dask_cuda/tests/test_proxy.py
+++ dask_cuda/tests/test_proxy.py
@@ -1,5 +1,6 @@
 import operator
 import pickle
+import sys
 from types import SimpleNamespace
 
 import numpy as np
@@ -20,6 +21,11 @@ from dask_cuda import proxy_object
 from dask_cuda.proxify_device_objects import proxify_device_objects
 from dask_cuda.proxify_host_file import ProxifyHostFile
 
+pytestmark = pytest.mark.skipif(
+    sys.version_info.minor < 8,
+    reason="Temporarily skipping some tests because of a bug "
+    "in Dask see <https://github.com/rapidsai/dask-cuda/issues/746>",
+)
 ProxifyHostFile.register_disk_spilling()  # Make the "disk" serializer available
 
 
