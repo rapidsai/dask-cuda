afaae1dcef9c60bdfd1d649528cdaf3f7c88ca2d
Mads R. B. Kristensen <madsbk@gmail.com>
2022-09-28 13:09:07 +0200

    Impl. VmmAllocPool

--- dask_cuda/rmm_vmm_pool.py
+++ dask_cuda/rmm_vmm_pool.py
@@ -1,3 +1,6 @@
+import traceback
+from typing import Dict
+
 from cuda import cuda, cudart, nvrtc
 
 import rmm.mr
@@ -28,7 +31,9 @@ def checkCudaErrors(result):
         msg = "CUDA error code={}({})".format(err.value, _cudaGetErrorEnum(err))
         if err == cuda.CUresult.CUDA_ERROR_OUT_OF_MEMORY:
             raise MemoryError(msg)
-        raise RuntimeError(msg)
+        raise RuntimeError(
+            msg + "\n" + "\n".join(str(frame) for frame in traceback.extract_stack())
+        )
 
     if len(result) == 1:
         return None
@@ -36,6 +41,16 @@ def checkCudaErrors(result):
         return result[1]
     else:
         return result[1:]
+
+
+def check_vmm_support(dev: cuda.CUdevice) -> None:
+    if not checkCudaErrors(
+        cuda.cuDeviceGetAttribute(
+            CU_VMM_SUPPORTED,
+            dev,
+        )
+    ):
+        raise ValueError("Device doesn't support VIRTUAL ADDRESS MANAGEMENT")
 
 
 def get_granularity(dev: cuda.CUdevice):
@@ -135,9 +150,93 @@ class VmmAlloc:
         checkCudaErrors(cuda.cuMemAddressFree(ptr, alloc_size))
 
 
+class VmmHeap:
+    def __init__(
+        self, device: cuda.CUdevice, granularity: int, size: int = 2**40
+    ) -> None:
+        self.granularity = granularity
+        self.device = device
+        self._offset = 0
+        self._heap = int(
+            checkCudaErrors(
+                cuda.cuMemAddressReserve(
+                    size=size, alignment=0, addr=cuda.CUdeviceptr(0), flags=0
+                )
+            )
+        )
+
+    def allocate(self, size: int) -> cuda.CUdeviceptr:
+        assert size % self.granularity == 0
+        ret = self._heap + self._offset
+        self._offset += to_aligned_size(size, self.granularity)
+        assert ret % self.granularity == 0
+        return cuda.CUdeviceptr(ret)
+
+
+class VmmAllocPool:
+    def __init__(self) -> None:
+        self._store: Dict[int, int] = {}
+        self._heaps: Dict[cuda.CUdevice, VmmHeap] = {}
+
+    def get_heap(self) -> VmmHeap:
+        checkCudaErrors(cudart.cudaGetDevice())  # TODO: avoid use of the Runtime API
+        device = checkCudaErrors(cuda.cuCtxGetDevice())
+        if device not in self._heaps:
+            check_vmm_support(device)
+            self._heaps[device] = VmmHeap(device, get_granularity(device))
+        return self._heaps[device]
+
+    def allocate(self, size: int) -> int:
+        heap = self.get_heap()
+        alloc_size = to_aligned_size(size, heap.granularity)
+
+        # Allocate physical memory
+        prop = cuda.CUmemAllocationProp()
+        prop.type = cuda.CUmemAllocationType.CU_MEM_ALLOCATION_TYPE_PINNED
+        prop.location.type = cuda.CUmemLocationType.CU_MEM_LOCATION_TYPE_DEVICE
+        prop.location.id = heap.device
+        mem_handle = checkCudaErrors(cuda.cuMemCreate(alloc_size, prop, 0))
+
+        # Map physical memory to the heap
+        reserve_ptr = heap.allocate(alloc_size)
+        checkCudaErrors(
+            cuda.cuMemMap(
+                ptr=reserve_ptr,
+                size=alloc_size,
+                offset=0,
+                handle=mem_handle,
+                flags=0,
+            )
+        )
+
+        # Since we do not need to make any other mappings of this memory or export it,
+        # we no longer need and can release the mem_handle.
+        # The allocation will be kept live until it is unmapped.
+        checkCudaErrors(cuda.cuMemRelease(mem_handle))
+
+        # Specify both read and write access.
+        prop = cuda.CUmemAccessDesc()
+        prop.location.type = cuda.CUmemLocationType.CU_MEM_LOCATION_TYPE_DEVICE
+        prop.location.id = heap.device
+        prop.flags = cuda.CUmemAccess_flags.CU_MEM_ACCESS_FLAGS_PROT_READWRITE
+        checkCudaErrors(
+            cuda.cuMemSetAccess(ptr=reserve_ptr, size=alloc_size, desc=[prop], count=1)
+        )
+
+        self._store[int(reserve_ptr)] = alloc_size
+        return int(reserve_ptr)
+
+    def deallocate(self, ptr: int, size: int) -> None:
+        checkCudaErrors(cudart.cudaGetDevice())  # TODO: avoid use of the Runtime API
+        alloc_size = self._store.pop(ptr)
+        checkCudaErrors(cuda.cuStreamSynchronize(cuda.CUstream_flags.CU_STREAM_DEFAULT))
+        checkCudaErrors(cuda.cuMemUnmap(ptr, alloc_size))
+
+
 def set_vmm_pool():
-    #store = RegularMemAlloc()
-    store = VmmAlloc()
+    # store = RegularMemAlloc()
+    # store = VmmAlloc()
+    store = VmmAllocPool()
 
     def allocate(size: int):
         return store.allocate(size)
