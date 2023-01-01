6ae8ccb8cd671362ab480d4284ef3308e0198639
AJ Schmidt <aschmidt@nvidia.com>
2023-01-01 12:56:52 -0500

    fix `build_python_pypi.sh`

--- ci/build_python_pypi.sh
+++ ci/build_python_pypi.sh
@@ -5,12 +5,11 @@ python -m pip install build --user
 
 # While conda provides these during conda-build, they are also necessary during
 # the setup.py build for PyPI
-echo "GIT_DESCRIBE_TAG=$(git describe --abbrev=0 --tags)" >> $GITHUB_ENV
-echo "GIT_DESCRIBE_NUMBER=$(git rev-list ${GIT_DESCRIBE_TAG}..HEAD --count)" >> $GITHUB_ENV
-# Compute version suffix
-source rapids-env-update
-echo "VERSION_SUFFIX=${VERSION_SUFFIX}" >> $GITHUB_ENV
+export GIT_DESCRIBE_TAG=$(git describe --abbrev=0 --tags)
+export GIT_DESCRIBE_NUMBER=$(git rev-list ${GIT_DESCRIBE_TAG}..HEAD --count)
 
+# Compute/export VERSION_SUFFIX
+source rapids-env-update
 
 python -m build \
   --sdist \
