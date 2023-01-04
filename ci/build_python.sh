d78c60aac4410c305fb462b1bc679889aec41e37
Bradley Dice <bdice@bradleydice.com>
2023-01-04 09:04:39 -0600

    Add GitHub Actions Workflows (#1062)
    This PR adds GitHub Actions workflows to `dask-cuda`.
    
    ### Task list
    
    Coverage required for this PR:
    - [x] Python tests
    - [x] Codecov
    - [x] Style checks
    
    Future work required:
    - [Deploy sdist/wheels to PyPI](https://github.com/rapidsai/dask-cuda/blob/d6ff68daae638c30e1e2e25f2fb91ecc1ee8f6ea/ci/cpu/build.sh#L98)
    
    Authors:
      - Bradley Dice (https://github.com/bdice)
      - AJ Schmidt (https://github.com/ajschmidt8)
    
    Approvers:
      - AJ Schmidt (https://github.com/ajschmidt8)
    
    URL: https://github.com/rapidsai/dask-cuda/pull/1062

---
+++ ci/build_python.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Copyright (c) 2022, NVIDIA CORPORATION.
+
+set -euo pipefail
+
+source rapids-env-update
+
+export CMAKE_GENERATOR=Ninja
+
+rapids-print-env
+
+rapids-logger "Begin py build"
+
+rapids-mamba-retry mambabuild \
+  conda/recipes/dask-cuda
+
+rapids-upload-conda-to-s3 python
