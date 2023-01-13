c6082a8983b5d77a00e4b3fed7d5feb19b0e8567
AJ Schmidt <aschmidt@nvidia.com>
2023-01-13 16:08:27 -0500

    add `-vv` to `pytest` cmd for debugging

--- ci/test_python.sh
+++ ci/test_python.sh
@@ -44,6 +44,7 @@ UCXPY_IFNAME=eth0 \
 UCX_WARN_UNUSED_ENV_VARS=n \
 UCX_MEMTYPE_CACHE=n \
 timeout 45m pytest \
+  -vv \
   --capture=no \
   --cache-clear \
   --junitxml="${RAPIDS_TESTS_DIR}/junit-dask-cuda.xml" \
