c2a959df581e8329795a54674ff95237e054d4db
jakirkham <jakirkham@gmail.com>
2023-06-28 00:59:38 -0700

    Preallocate memory before read
    Before doing more expensive IO operations, allocate all memory needed in
    one go. This way it can be fed into the IO operations rapidly or be batched.

--- dask_cuda/disk_io.py
+++ dask_cuda/disk_io.py
@@ -206,20 +206,22 @@ def disk_read(header: Mapping, gds=False) -> list:
     if gds:
         import kvikio  # isort:skip
 
+        ret.extend(
+            get_new_cuda_buffer()(length)
+            if is_cuda
+            else np.empty((length,), dtype="u1")
+            for length, is_cuda in zip(header["frame-lengths"], header["cuda-frames"])
+        )
         with kvikio.CuFile(header["path"], "rb") as f:
             file_offset = 0
-            for length, is_cuda in zip(header["frame-lengths"], header["cuda-frames"]):
-                if is_cuda:
-                    buf = get_new_cuda_buffer()(length)
-                else:
-                    buf = np.empty((length,), dtype="u1")
-                f.pread(buf, file_offset=file_offset).get()
-                file_offset += length
-                ret.append(buf)
+            for b in ret:
+                f.pread(b, file_offset=file_offset).get()
+                file_offset += b.nbytes
     else:
+        ret.extend(
+            np.empty((length,), dtype="u1") for length in header["frame-lengths"]
+        )
         with open(str(header["path"]), "rb") as f:
-            for length in header["frame-lengths"]:
-                buf = np.empty((length,), dtype="u1")
-                f.readinto(buf)
-                ret.append(buf)
+            for b in ret:
+                f.readinto(b)
     return ret
