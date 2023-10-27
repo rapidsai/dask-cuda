d9e10013afb5501f1438d2a7f8d0394368e30108
GALI PREM SAGAR <sagarprem75@gmail.com>
2023-10-27 17:03:32 -0500

    Generate proper, consistent nightly versions for pip and conda packages (#1267)
    This PR changes conda Python packages and wheels to all generate a consistent version for nightlies. The nightly version is of the form YY.MM.DDaN, where N is the number of commits from the last tag. The version is embedded in both the package metadata and in the `dask_cuda.__version__` attribute. In addition the commit hash itself is embedded into the package as `dask_cuda.__git_commit__`. These changes ensure that
    
    1. wheels are properly considered nightlies and are treated accordingly by pip (e.g. requiring --pre for installation, not conflicting with normal releases, etc)
    2. wheels and conda packages are aligned on versions so that they can be easily compared if necessary.
    
    Authors:
      - GALI PREM SAGAR (https://github.com/galipremsagar)
    
    Approvers:
      - Vyas Ramasubramani (https://github.com/vyasr)
      - https://github.com/jakirkham
      - Ray Douglass (https://github.com/raydouglass)
    
    URL: https://github.com/rapidsai/dask-cuda/pull/1267

--- ci/build_python_pypi.sh
+++ ci/build_python_pypi.sh
@@ -3,6 +3,9 @@
 
 python -m pip install build --user
 
+
+version=$(rapids-generate-version)
+commit=$(git rev-parse HEAD)
 # While conda provides these during conda-build, they are also necessary during
 # the setup.py build for PyPI
 export GIT_DESCRIBE_TAG=$(git describe --abbrev=0 --tags)
@@ -11,25 +14,19 @@ export GIT_DESCRIBE_NUMBER=$(git rev-list ${GIT_DESCRIBE_TAG}..HEAD --count)
 # Build date for PyPI pre-releases using version from `pyproject.toml` as source.
 TOML_VERSION=$(grep "version = .*" pyproject.toml | grep -o '".*"' | sed 's/"//g')
 if ! rapids-is-release-build; then
-  export BUILD_DATE=$(date +%y%m%d)
-  export PACKAGE_VERSION_NUMBER="${TOML_VERSION}a${BUILD_DATE}"
+  export PACKAGE_VERSION_NUMBER="${version}"
 fi
+
+
+echo "${version}" | tr -d '"' > VERSION
+sed -i "/^__git_commit__/ s/= .*/= \"${commit}\"/g" "${package_name}/_version.py"
 
 # Compute/export RAPIDS_DATE_STRING
 source rapids-env-update
 
-# Update pyproject.toml with pre-release build date
-if ! rapids-is-release-build; then
-  sed -i "s/^version = \""${TOML_VERSION}".*\"/version = \""${PACKAGE_VERSION_NUMBER}"\"/g" pyproject.toml
-fi
 
 python -m build \
   --sdist \
   --wheel \
   --outdir dist/ \
   .
-
-# Revert pyproject.toml pre-release build date
-if ! rapids-is-release-build; then
-  sed -i "s/^version = \""${PACKAGE_VERSION_NUMBER}"\"/version = \""${TOML_VERSION}"\"/g" pyproject.toml
-fi
