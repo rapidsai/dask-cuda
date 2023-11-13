07fc9021beec06d68f7cac8d2c978c9c67bf8e11
Vyas Ramasubramani <vyasr@nvidia.com>
2023-11-13 15:59:35 -0800

    Add missing alpha spec

--- ci/build_python_pypi.sh
+++ ci/build_python_pypi.sh
@@ -17,6 +17,16 @@ if ! rapids-is-release-build; then
   export PACKAGE_VERSION_NUMBER="${version}"
 fi
 
+# For nightlies we want to ensure that we're pulling in alphas as well. The
+# easiest way to do so is to augment the spec with a constraint containing a
+# min alpha version that doesn't affect the version bounds but does allow usage
+# of alpha versions for that dependency without --pre
+alpha_spec=''
+if ! rapids-is-release-build; then
+    alpha_spec=',>=0.0.0a0'
+fi
+
+sed -r -i "s/rapids-dask-dependency==(.*)\"/rapids-dask-dependency==\1${alpha_spec}\"/g" pyproject.toml
 
 echo "${version}" | tr -d '"' > VERSION
 sed -i "/^__git_commit__/ s/= .*/= \"${commit}\"/g" "${package_name}/_version.py"
