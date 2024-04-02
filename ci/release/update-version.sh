19522fdc42e4685c9a30c87e6d52e7e63eaedcbe
Peter Andreas Entschev <peter@entschev.com>
2024-04-02 09:19:34 -0700

    Update docs files recursively

--- ci/release/update-version.sh
+++ ci/release/update-version.sh
@@ -58,6 +58,6 @@ done
 sed_runner "s/RAPIDS_VERSION_NUMBER=\".*/RAPIDS_VERSION_NUMBER=\"${NEXT_SHORT_TAG}\"/g" ci/build_docs.sh
 
 # Docs referencing source code
-for FILE in docs/source/*.rst; do
+find docs/source/ -type f -name *.rst -print0 | while IFS= read -r -d '' filename; do
     sed_runner "s|/branch-[^/]*/|/branch-${NEXT_SHORT_TAG}/|g" "${FILE}"
 done
