040d2ae5cf9525303824a179554dac1e297a36d7
James Lamb <jlamb@nvidia.com>
2024-07-24 14:25:16 -0500

    pre-commit

--- ci/release/update-version.sh
+++ ci/release/update-version.sh
@@ -58,7 +58,7 @@ UCX_DEPENDENCIES=(
   ucxx
 )
 for DEP in "${UCX_DEPENDENCIES[@]}"; do
-  for FILE in dependencies.yaml conda/environments/*.yaml; do 
+  for FILE in dependencies.yaml conda/environments/*.yaml; do
     sed_runner "/-.* ${DEP}\(-cu[[:digit:]]\{2\}\)\{0,1\}==/ s/==.*/==${NEXT_UCXPY_VERSION}.*,>=0.0.0a0/g" "${FILE}"
   done
   sed_runner "/\"${DEP}==/ s/==.*\"/==${NEXT_UCXPY_VERSION}.*,>=0.0.0a0\"/g" pyproject.toml
