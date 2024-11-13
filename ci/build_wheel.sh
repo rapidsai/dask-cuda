777033eb08838aeae517110fe5b5637b8a01d60d
James Lamb <jlamb@nvidia.com>
2024-11-13 00:02:06 -0600

    enforce wheel size limits, README formatting in CI

--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -8,5 +8,6 @@ source rapids-date-string
 rapids-generate-version > ./VERSION
 
 python -m pip wheel . -w dist -v --no-deps --disable-pip-version-check
+./ci/validate_wheel.sh dist
 
 RAPIDS_PY_WHEEL_NAME="dask-cuda" rapids-upload-wheels-to-s3 dist
