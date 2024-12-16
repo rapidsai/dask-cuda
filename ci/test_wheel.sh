4ab1ed0765919311584b2b20f123ede3ff14cae9
Gil Forsyth <gforsyth@nvidia.com>
2024-12-16 09:55:11 -0500

    chore: fix wheel name generation
    We want a name like:
    
    `dask_cuda_wheel_python_dask_cuda_py312_x86_64.tar.gz`
    
    from these environment variables

--- ci/test_wheel.sh
+++ ci/test_wheel.sh
@@ -3,8 +3,7 @@
 
 set -eou pipefail
 
-RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
-RAPIDS_PY_WHEEL_NAME="dask_cuda-${RAPIDS_PY_CUDA_SUFFIX}" RAPIDS_PY_WHEEL_PURE="1" rapids-download-wheels-from-s3 ./dist
+RAPIDS_PY_VERSION="312" RAPIDS_PY_WHEEL_NAME="dask-cuda" rapids-download-wheels-from-s3 ./dist
 
 # echo to expand wildcard before adding `[extra]` requires for pip
 python -m pip install $(echo ./dist/dask_cuda*.whl)[test]
