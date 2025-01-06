a1d78742a2e26ee04daafadadd91f425c7d0a340
Gil Forsyth <gforsyth@users.noreply.github.com>
2025-01-06 16:02:07 -0500

    Add wheel tests to CI (#1416)
    Adding this now that wheels are available
    
    - **deps(kvikio): add kvikio to CUDA version matrices**
    - **test(wheels): enable wheel tests in CI**
    
    Resolves #1344
    
    Authors:
      - Gil Forsyth (https://github.com/gforsyth)
    
    Approvers:
      - Peter Andreas Entschev (https://github.com/pentschev)
      - James Lamb (https://github.com/jameslamb)
    
    URL: https://github.com/rapidsai/dask-cuda/pull/1416

---
+++ ci/test_wheel.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Copyright (c) 2025, NVIDIA CORPORATION.
+
+set -eou pipefail
+
+RAPIDS_PY_WHEEL_NAME="dask-cuda" RAPIDS_PY_WHEEL_PURE="1" rapids-download-wheels-from-s3 python ./dist
+
+# Install cuda-suffixed dependencies b/c while `dask-cuda` has no cuda suffix, the test dependencies do
+rapids-dependency-file-generator \
+    --output requirements \
+    --file-key "test_python" \
+    --matrix "cuda=${RAPIDS_CUDA_VERSION%.*};arch=$(arch);py=${RAPIDS_PY_VERSION};cuda_suffixed=true" \
+| tee /tmp/requirements-test.txt
+
+rapids-logger "Installing test dependencies"
+# echo to expand wildcard
+python -m pip install -v --prefer-binary -r /tmp/requirements-test.txt $(echo ./dist/dask_cuda*.whl)
+
+python -m pytest ./dask_cuda/tests -k "not ucxx"
