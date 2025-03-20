9211b3851aa14584db35e4ba4d4cc3ccc9da234a
VenkateshJaya <jjayabaskar@nvidia.com>
2025-03-20 13:26:22 -0700

    added wheel build location

--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -3,11 +3,13 @@
 
 set -euo pipefail
 
+wheel_dir=${RAPIDS_WHEEL_BLD_OUTPUT_DIR}
+
 source rapids-date-string
 
 rapids-generate-version > ./VERSION
 
-rapids-pip-retry wheel . -w dist -v --no-deps --disable-pip-version-check
-./ci/validate_wheel.sh dist
+rapids-pip-retry wheel . -w "${wheel_dir}" -v --no-deps --disable-pip-version-check
+./ci/validate_wheel.sh "${wheel_dir}"
 
-RAPIDS_PY_WHEEL_NAME="dask-cuda" RAPIDS_PY_WHEEL_PURE="1" rapids-upload-wheels-to-s3 dist
+RAPIDS_PY_WHEEL_NAME="dask-cuda" RAPIDS_PY_WHEEL_PURE="1" rapids-upload-wheels-to-s3 python "${wheel_dir}"
