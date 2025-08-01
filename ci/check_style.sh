d721afc7f53271c5cf1290b8d1994a79da8e9b78
Kyle Edwards <kyedwards@nvidia.com>
2025-08-01 15:36:35 -0400

    Download in check_style

--- ci/check_style.sh
+++ ci/check_style.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (c) 2020-2022, NVIDIA CORPORATION.
+# Copyright (c) 2020-2025, NVIDIA CORPORATION.
 
 set -euo pipefail
 
@@ -10,6 +10,12 @@ rapids-dependency-file-generator \
   --output conda \
   --file-key checks \
   --matrix "cuda=${RAPIDS_CUDA_VERSION%.*};arch=$(arch);py=${RAPIDS_PY_VERSION}" | tee env.yaml
+
+mkdir /tmp/rbb
+pushd /tmp/rbb
+wget -O ../rbb.zip https://github.com/rapidsai/rapids-build-backend/actions/runs/16683467298/artifacts/3670230978
+unzip ../rbb.zip
+popd
 
 rapids-mamba-retry env create --yes -f env.yaml -n checks
 conda activate checks
