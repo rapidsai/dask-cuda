c69d94f75b6133536e03e0bc91f19229f1db000a
Gil Forsyth <gforsyth@nvidia.com>
2025-11-14 14:47:17 -0500

    feat(wheels): restrict pure wheel installation to linux only

--- ci/validate_wheel.sh
+++ ci/validate_wheel.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
-# SPDX-FileCopyrightText: Copyright (c) 2024, NVIDIA CORPORATION.
+# SPDX-FileCopyrightText: Copyright (c) 2024-2025, NVIDIA CORPORATION.
 # SPDX-License-Identifier: Apache-2.0
 
 set -euo pipefail
 
 wheel_dir_relative_path=$1
+
+# Pure wheels can be installed on any OS and we want to avoid users being able
+# to begin installing them on Windows or OSX when we know that the dependencies
+# won't work / be available.
+for wheel in "${wheel_dir_relative_path}"/*-py3-none-any.whl; do
+    if [ -f "${wheel}" ]; then
+        rapids-logger "Retagging pure Python wheel: ${wheel}"
+
+        # Retag for manylinux x86_64 and manylinux aarch64
+        wheel tags --platform-tag=manylinux_2_5_x86_64.manylinux_2_5_aarch64 --remove "${wheel}"
+
+        rapids-logger "Successfully retagged wheel for manylinux platforms"
+    fi
+done
 
 rapids-logger "validate packages with 'pydistcheck'"
 
