1fa70bce448ca84bdb1b8e5f563ef8118c073ca0
brandon-b-miller <53796099+brandon-b-miller@users.noreply.github.com>
2025-12-10 09:42:08 -0600

    Restore tests from `numba-cuda` upgrade (#1595)
    Follow up to https://github.com/rapidsai/dask-cuda/pull/1591, this PR restores tests now that a cuDF package with a matching `numba-cuda` dependency can be found.
    
    Authors:
      - https://github.com/brandon-b-miller
    
    Approvers:
      - Peter Andreas Entschev (https://github.com/pentschev)
      - Bradley Dice (https://github.com/bdice)
    
    URL: https://github.com/rapidsai/dask-cuda/pull/1595

--- ci/test_wheel.sh
+++ ci/test_wheel.sh
@@ -1,3 +1,41 @@
 #!/bin/bash
-# SPDX-FileCopyrightText: Copyright (c) 2025, NVIDIA CORPORATION. All rights reserved.
+# SPDX-FileCopyrightText: Copyright (c) 2025, NVIDIA CORPORATION.
 # SPDX-License-Identifier: Apache-2.0
+
+set -eou pipefail
+
+source rapids-init-pip
+
+RAPIDS_CUDA_MAJOR="${RAPIDS_CUDA_VERSION%%.*}"
+export RAPIDS_CUDA_MAJOR
+
+DASK_CUDA_WHEELHOUSE=$(rapids-download-from-github "$(rapids-package-name "wheel_python" "dask-cuda" --pure)")
+
+rapids-logger "Installing test dependencies"
+# echo to expand wildcard
+rapids-pip-retry install \
+  -v \
+  --prefer-binary \
+  "$(echo "${DASK_CUDA_WHEELHOUSE}"/dask_cuda*.whl)[cu${RAPIDS_CUDA_MAJOR},test,test-cu${RAPIDS_CUDA_MAJOR}]"
+
+RAPIDS_TESTS_DIR=${RAPIDS_TESTS_DIR:-"${PWD}/test-results"}
+mkdir -p "${RAPIDS_TESTS_DIR}"
+
+EXITCODE=0
+# shellcheck disable=SC2329
+set_exit_code() {
+    EXITCODE=$?
+    rapids-logger "Test failed with error ${EXITCODE}"
+}
+trap set_exit_code ERR
+set +e
+
+rapids-logger "pytest dask-cuda"
+./ci/run_pytest.sh \
+  --junitxml="${RAPIDS_TESTS_DIR}/junit-dask-cuda.xml"
+
+rapids-logger "Run local benchmark"
+./ci/run_benchmarks.sh
+
+rapids-logger "Test script exiting with latest error code: $EXITCODE"
+exit ${EXITCODE}
