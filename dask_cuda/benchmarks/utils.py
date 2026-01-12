292c2223bf63896796ec1a6aa95c1c8862b3ab5e
Peter Andreas Entschev <peter@entschev.com>
2026-01-12 07:55:28 -0800

    Update copyright year

--- dask_cuda/benchmarks/utils.py
+++ dask_cuda/benchmarks/utils.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: Copyright (c) 2020-2025, NVIDIA CORPORATION & AFFILIATES.
+# SPDX-FileCopyrightText: Copyright (c) 2020-2026, NVIDIA CORPORATION & AFFILIATES.
 # SPDX-License-Identifier: Apache-2.0
 
 import argparse
