174f7aecc6d1d3a894f40473895a672a5dd8cad1
Mike McCarty <mmccarty@nvidia.com>
2026-01-16 13:20:49 -0500

    fix for changes in pip's handling of build constraints

--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# SPDX-FileCopyrightText: Copyright (c) 2023-2025, NVIDIA CORPORATION.
+# SPDX-FileCopyrightText: Copyright (c) 2023-2026, NVIDIA CORPORATION.
 # SPDX-License-Identifier: Apache-2.0
 
 set -euo pipefail
@@ -9,7 +9,29 @@ source rapids-init-pip
 
 rapids-generate-version > ./VERSION
 
-rapids-pip-retry wheel . -w "${RAPIDS_WHEEL_BLD_OUTPUT_DIR}" -v --no-deps --disable-pip-version-check
+RAPIDS_PIP_WHEEL_ARGS=(
+  -w "${RAPIDS_WHEEL_BLD_OUTPUT_DIR}"
+  -v
+  --no-deps
+  --disable-pip-version-check
+)
+
+# Only use --build-constraint when build isolation is enabled.
+#
+# Passing '--build-constraint' and '--no-build-isolation` together results in an error from 'pip',
+# but we want to keep environment variable PIP_CONSTRAINT set unconditionally.
+# PIP_NO_BUILD_ISOLATION=0 means "add --no-build-isolation" (ref: https://github.com/pypa/pip/issues/573
+if [[ "${PIP_NO_BUILD_ISOLATION:-}" != "0" ]]; then
+    RAPIDS_PIP_WHEEL_ARGS+=(--build-constraint="${PIP_CONSTRAINT}")
+fi
+
+# unset PIP_CONSTRAINT (set by rapids-init-pip)... it doesn't affect builds as of pip 25.3, and
+# results in an error from 'pip wheel' when set and --build-constraint is also passed
+unset PIP_CONSTRAINT
+rapids-pip-retry wheel \
+    "${RAPIDS_PIP_WHEEL_ARGS[@]}" \
+    .
+
 ./ci/validate_wheel.sh "${RAPIDS_WHEEL_BLD_OUTPUT_DIR}"
 
 RAPIDS_PACKAGE_NAME="$(rapids-package-name wheel_python dask-cuda --pure)"
