2f543e6cb8e547d24116c3f8ec37d3ed82359bf4
James Lamb <jaylamb20@gmail.com>
2026-02-05 11:14:46 -0600

    remove pip.conf migration code in CI scripts (#1622)
    Follow-up to changes for https://github.com/rapidsai/build-planning/issues/241
    
    * removes `rapids-init-pip` feature flags now that those have become the default behavior (https://github.com/rapidsai/gha-tools/pull/242)
    
    Authors:
      - James Lamb (https://github.com/jameslamb)
    
    Approvers:
      - Gil Forsyth (https://github.com/gforsyth)
    
    URL: https://github.com/rapidsai/dask-cuda/pull/1622

--- ci/test_wheel.sh
+++ ci/test_wheel.sh
@@ -4,8 +4,6 @@
 
 set -eou pipefail
 
-RAPIDS_INIT_PIP_REMOVE_NVIDIA_INDEX="true"
-export RAPIDS_INIT_PIP_REMOVE_NVIDIA_INDEX
 source rapids-init-pip
 
 RAPIDS_CUDA_MAJOR="${RAPIDS_CUDA_VERSION%%.*}"
